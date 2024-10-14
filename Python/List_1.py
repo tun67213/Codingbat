3e8e8121283c418c250b9fc1bba45fc5776b866f
tun67213 <tun67213@temple.edu>
2024-10-14 07:23:57 -0400

    Created a comment for implementation details for one of the functions

---
+++ Python/List_1.py
@@ -0,0 +1,11 @@
+
+def first_last6(nums):
+    """
+    Finds out if the first or last value in the provided array is a 6 and returns true if so. False otherwise.
+
+    Parameters:
+        nums (int[]): array of values to provide this function with
+
+    Returns:
+        True if the first or last value in the provided array is 6, False otherwise.
+    """
