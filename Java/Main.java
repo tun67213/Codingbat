d099effe8c6754408646fe5944d18a11b704bb63
tun67213 <tun67213@temple.edu>
2024-10-14 09:28:17 -0400

    Created class to run all main() functions in

---
+++ Java/Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author arvindhvelrajan
+ * @code Function to run all main programs for Java code in Codingbat.com
+ */
+public class Main
+{
+	/**
+	 * @param args command line arguments for this function
+	 */
+	public static void main(String[] args)
+	{
+		
+	}
+}
