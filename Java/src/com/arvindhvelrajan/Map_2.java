517b702ecfd5a7311d2df9f651ec8be09df8239d
tun67213 <tun67213@temple.edu>
2024-11-28 10:35:50 -0500

    Starting over

--- Java/src/com/arvindhvelrajan/Map_2.java
+++
@@ -1,6 +0,0 @@
-package com.arvindhvelrajan;
-
-import static com.arvindhvelrajan.UI_Functions.*;
-
-public class Map_2 {
-}
--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++
@@ -1,6 +0,0 @@
-package com.arvindhvelrajan;
-
-import static com.arvindhvelrajan.UI_Functions.*;
-
-public class Warmup_1 {
-}
