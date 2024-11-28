f7734516af20315a0ab1ab25e341007e69de56b6
tun67213 <tun67213@temple.edu>
2024-11-28 10:41:51 -0500

    Created class and imported UI_Functions

---
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -0,0 +1,12 @@
+package com.arvindhvelrajan;
+
+import static com.arvindhvelrajan.UI_Functions.*;
+
+/**
+ * @author arvindhvelrajan
+ * @code This class implements all functions in Warmup-1 from codingbat.com
+ */
+public class Warmup_1
+{
+	
+}
