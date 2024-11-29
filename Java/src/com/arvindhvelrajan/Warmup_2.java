368fb86f4894f09e832ef5b1bec2dd25e3485868
tun67213 <tun67213@temple.edu>
2024-11-29 09:10:14 -0500

    Imported printing functions from other file

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -1,5 +1,7 @@
 package com.arvindhvelrajan;
 
+import static com.arvindhvelrajan.UI_Functions.*;
+
 /**
  * @author arvindhvelrajan
  * @code This class implements all functions in Warmup-2 from codingbat.com
