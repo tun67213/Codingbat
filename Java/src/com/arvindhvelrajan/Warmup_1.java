3a47e7a5960c8c88e902560c2adfeb9cac6f3aa9
tun67213 <tun67213@temple.edu>
2024-11-29 15:52:13 -0500

    Added monkeyTrouble()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -26,6 +26,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+	 *
+	 * monkeyTrouble(true, true) → true
+	 * monkeyTrouble(false, false) → true
+	 * monkeyTrouble(true, false) → false
+	 */
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
