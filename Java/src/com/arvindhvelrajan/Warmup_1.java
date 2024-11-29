ee2acd7d715219c6d2cb82ed53eced830f9ad494
tun67213 <tun67213@temple.edu>
2024-11-29 15:59:53 -0500

    Added parrotTrouble() function

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -62,6 +62,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+	 *
+	 * parrotTrouble(true, 6) → true
+	 * parrotTrouble(true, 7) → false
+	 * parrotTrouble(false, 6) → false
+	 */
+	public boolean parrotTrouble(boolean talking, int hour)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
