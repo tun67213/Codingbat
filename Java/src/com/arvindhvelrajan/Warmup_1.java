9a98fe7e91b1a58382e867e913f0c97a620f80a9
tun67213 <tun67213@temple.edu>
2024-11-29 15:53:53 -0500

    Added function sumDouble()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -38,6 +38,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+	 *
+	 * sumDouble(1, 2) → 3
+	 * sumDouble(3, 2) → 5
+	 * sumDouble(2, 2) → 8
+	 */
+	public int sumDouble(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
