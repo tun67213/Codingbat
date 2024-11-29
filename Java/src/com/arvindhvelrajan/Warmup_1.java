88fdf097edbcad63bb0b9a0a26fd1286c9aefa01
tun67213 <tun67213@temple.edu>
2024-11-29 18:45:14 -0500

    Added function posNeg()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -106,6 +106,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+	 *
+	 * posNeg(1, -1, false) → true
+	 * posNeg(-1, 1, false) → true
+	 * posNeg(-4, -5, true) → true
+	 */
+	public boolean posNeg(int a, int b, boolean negative)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
