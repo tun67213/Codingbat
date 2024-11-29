d4c106fff6267a3eea0c2de0c65184f376dfd54b
tun67213 <tun67213@temple.edu>
2024-11-29 15:56:52 -0500

    Added diff21()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -50,6 +50,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+	 *
+	 * diff21(19) → 2
+	 * diff21(10) → 11
+	 * diff21(21) → 0
+	 */
+	public int diff21(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
