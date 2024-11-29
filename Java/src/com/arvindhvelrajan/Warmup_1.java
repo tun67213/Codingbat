a5ba258b0577f84f9f92d7d9ccc99184c07a8317
tun67213 <tun67213@temple.edu>
2024-11-29 00:15:27 -0500

    Created helper function inRange() to implement in1020()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -308,6 +308,14 @@ public class Warmup_1
 	}
 
 	/**
+	 * This private helper function will be used to check if the provided value is within range or not.
+	 */
+	private boolean inRange(int a)
+	{
+		return a >= 10 && a <= 20;
+	}
+
+	/**
 	 * We'll say that a number is "teen" if it is in the range 13..19 inclusive.
 	 * Given 3 int values, return true if 1 or more of them are teen.
 	 *
