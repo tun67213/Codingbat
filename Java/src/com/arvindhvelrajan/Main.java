17d04d6fcfbcd639d3068e4e85d96eef783e6722
tun67213 <tun67213@temple.edu>
2024-11-28 23:31:42 -0500

    Added welcome() function and called warmup-1 functions

--- Java/src/com/arvindhvelrajan/Main.java
+++ Java/src/com/arvindhvelrajan/Main.java
@@ -6,8 +6,14 @@ package com.arvindhvelrajan;
  */
 public class Main
 {
+	public static void welcome()
+	{
+		System.out.println("Welcome to Codingbat.com Java Function Implementation!\n");
+	}
 	public static void main(String[] args)
 	{
-		
+		welcome();
+		Warmup_1 warmup1 = new Warmup_1();
+		warmup1.main(args);
 	}
 }
