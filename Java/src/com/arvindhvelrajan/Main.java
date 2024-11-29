924f545a9a97774c9df9534d8e68b142251d23bb
tun67213 <tun67213@temple.edu>
2024-11-28 22:29:12 -0500

    Created file to run all classes

---
+++ Java/src/com/arvindhvelrajan/Main.java
@@ -0,0 +1,13 @@
+package com.arvindhvelrajan;
+
+/**
+ * @author arvindhvelrajan
+ * @code This is the main function to test all non-static classes in this Project
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
