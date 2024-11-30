93b6e36c2b0efc7a697c76de64bcc0bf20519ea9
tun67213 <tun67213@temple.edu>
2024-11-30 07:26:16 -0500

    Updated comment so that it isn't extensively long

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -17,7 +17,8 @@ public class Warmup_2
 	}
 
 	/**
-	 * Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+	 * Given a string and a non-negative int n, return a larger string that is n copies
+	 * of the original string.
 	 *
 	 * stringTimes("Hi", 2) → "HiHi"
 	 * stringTimes("Hi", 3) → "HiHiHi"
