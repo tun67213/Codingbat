735fc4fae145f7e1fcdd3d794160da7c0e5fe47b
tun67213 <tun67213@temple.edu>
2024-11-29 20:37:13 -0500

    Added function backAround()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -172,6 +172,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given a string, take the last char and return a new string with the last char added at the front and back, so "cat" yields "tcatt". The original string will be length 1 or more.
+	 *
+	 * backAround("cat") → "tcatt"
+	 * backAround("Hello") → "oHelloo"
+	 * backAround("a") → "aaa"
+	 */
+	public String backAround(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
