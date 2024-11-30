3b4508753819576d68c08b7db8c9d8e9e4ccdcfd
tun67213 <tun67213@temple.edu>
2024-11-30 07:33:59 -0500

    Added function doubleX()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -55,6 +55,19 @@ public class Warmup_2
 	}
 
 	/**
+	 * Given a string, return true if the first instance of "x" in the string is immediately followed
+	 * by another "x".
+	 *
+	 * doubleX("axxbb") → true
+	 * doubleX("axaxax") → false
+	 * doubleX("xxxxx") → true
+	 */
+	public boolean doubleX(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
