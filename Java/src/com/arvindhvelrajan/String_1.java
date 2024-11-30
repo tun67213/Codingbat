ea45cfbf402c1603e84135cf44ddae2488bfb29e
tun67213 <tun67213@temple.edu>
2024-11-30 16:31:15 -0500

    Split JavaDoc into two lines for easy readability

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -161,7 +161,8 @@ public class String_1
 	}
 
 	/**
-	 * Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
+	 * Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start.
+	 * The string length will be at least 2.
 	 *
 	 * right2("Hello") → "loHel"
 	 * right2("java") → "vaja"
