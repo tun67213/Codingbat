dbe27fb37fa76f60a3e2a38135e863b084c8ef84
tun67213 <tun67213@temple.edu>
2024-11-29 21:00:58 -0500

    Added function hasTeen()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -249,6 +249,17 @@ public class Warmup_1
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
+	/**
+	 * We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+	 *
+	 * hasTeen(13, 20, 10) → true
+	 * hasTeen(20, 19, 10) → true
+	 * hasTeen(20, 10, 13) → true
+	 */
+	public boolean hasTeen(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
 	/**
 	 * @param args commandline arguments
