954830875be203a4a9cf2da1e319ee7d086ec9e7
tun67213 <tun67213@temple.edu>
2024-11-30 16:31:57 -0500

    Added function theEnd()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -174,6 +174,19 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return a string length 1 from its front, unless front is false, in which case return a
+	 * string length 1 from its back. The string will be non-empty.
+	 *
+	 * theEnd("Hello", true) → "H"
+	 * theEnd("Hello", false) → "o"
+	 * theEnd("oh", true) → "o"
+	 */
+	public String theEnd(String str, boolean front)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
