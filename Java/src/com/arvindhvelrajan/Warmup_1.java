56477c31070493ca226eaca62a5326baf9d70f15
tun67213 <tun67213@temple.edu>
2024-11-29 20:40:37 -0500

    Added function or35()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -187,6 +187,19 @@ public class Warmup_1
 	}
 
 	/**
+	 * Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
+	 * Use the % "mod" operator -- see Introduction to Mod
+	 *
+	 * or35(3) → true
+	 * or35(10) → true
+	 * or35(8) → false
+	 */
+	public boolean or35(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
