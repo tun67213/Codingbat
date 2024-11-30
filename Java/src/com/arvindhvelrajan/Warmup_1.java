9b87b3836953f537951373bbc0709a5fe4af50c3
tun67213 <tun67213@temple.edu>
2024-11-29 21:04:04 -0500

    Split comment for hasTeen() into two because it was too long

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -250,7 +250,8 @@ public class Warmup_1
 	}
 
 	/**
-	 * We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+	 * We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+	 * Given 3 int values, return true if 1 or more of them are teen.
 	 *
 	 * hasTeen(13, 20, 10) → true
 	 * hasTeen(20, 19, 10) → true
