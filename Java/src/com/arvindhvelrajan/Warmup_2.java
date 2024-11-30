5317b9aaef518b98dae2024011e3e5679a6962e0
tun67213 <tun67213@temple.edu>
2024-11-30 07:20:25 -0500

    Added JavaDoc for welcome()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -8,6 +8,9 @@ import static com.arvindhvelrajan.UI_Functions.*;
  */
 public class Warmup_2
 {
+	/**
+	 * This function introduces the user to the class that is being demonstrated.
+	 */
 	private void welcome()
 	{
 		System.out.println("\n\nWarmup-2 Functions\n");
