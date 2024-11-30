c9df8e827190813612cdf4ad9d32987016687d1a
tun67213 <tun67213@temple.edu>
2024-11-30 17:20:50 -0500

    Added function middleTwo()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -200,6 +200,19 @@ public class String_1
 	}
 
 	/**
+	 * Given a string of even length, return a string made of the middle two chars, so the
+	 * string "string" yields "ri". The string length will be at least 2.
+	 *
+	 * middleTwo("string") → "ri"
+	 * middleTwo("code") → "od"
+	 * middleTwo("Practice") → "ct"
+	 */
+	public String middleTwo(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
