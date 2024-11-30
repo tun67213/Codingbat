917aa1b027060f280b2982ff1b05348f9d44cb95
tun67213 <tun67213@temple.edu>
2024-11-29 21:25:07 -0500

    Added function in3050()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -340,6 +340,19 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given 2 int values, return true if they are both in the range 30..40 inclusive,
+	 * or they are both in the range 40..50 inclusive.
+	 *
+	 * in3050(30, 31) → true
+	 * in3050(30, 41) → false
+	 * in3050(40, 50) → true
+	 */
+	public boolean in3050(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
