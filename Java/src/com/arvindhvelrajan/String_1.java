7e7bd1ebdd6d98e5dbeccdf3dc256520de8dfc0a
tun67213 <tun67213@temple.edu>
2024-11-30 17:25:45 -0500

    Added function nTwice()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -225,6 +225,19 @@ public class String_1
 	}
 
 	/**
+	 * Given a string and an int n, return a string made of the first and last n chars from the string.
+	 * The string length will be at least n.
+	 *
+	 * nTwice("Hello", 2) → "Helo"
+	 * nTwice("Chocolate", 3) → "Choate"
+	 * nTwice("Chocolate", 1) → "Ce"
+	 */
+	public String nTwice(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
