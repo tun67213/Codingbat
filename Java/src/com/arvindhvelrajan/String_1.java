308fc1d97cdd126797a2f57a701c64c820c73ed3
tun67213 <tun67213@temple.edu>
2024-11-30 09:26:16 -0500

    Added function makeOutWord()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -56,6 +56,21 @@ public class String_1
 	}
 
 	/**
+	 * Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the
+	 * middle of the out string, e.g. "<<word>>".
+	 * Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including
+	 * index j.
+	 *
+	 * makeOutWord("<<>>", "Yay") → "<<Yay>>"
+	 * makeOutWord("<<>>", "WooHoo") → "<<WooHoo>>"
+	 * makeOutWord("[[]]", "word") → "[[word]]"
+	 */
+	public String makeOutWord(String out, String word)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
