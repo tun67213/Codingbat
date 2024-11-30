e797dc8c9d187f0868ce50481f21ec4ef24a66cc
tun67213 <tun67213@temple.edu>
2024-11-30 08:03:11 -0500

    Added function stringMatch()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -143,6 +143,20 @@ public class Warmup_2
 	}
 
 	/**
+	 * Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
+	 * So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in
+	 * both strings.
+	 *
+	 * stringMatch("xxcaazz", "xxbaaz") → 3
+	 * stringMatch("abc", "abc") → 2
+	 * stringMatch("abc", "axc") → 0
+	 */
+	public int stringMatch(String a, String b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
