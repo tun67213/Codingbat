0da5424dab24ca90e6b05f7c534eede5a7893eb5
tun67213 <tun67213@temple.edu>
2024-11-30 18:41:56 -0500

    Updated JavaDoc for lastTwo() for readability

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -320,7 +320,8 @@ public class String_1
 	}
 
 	/**
-	 * Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+	 * Given a string of any length, return a new string where the last 2 chars, if present, are swapped,
+	 * so "coding" yields "codign".
 	 *
 	 * lastTwo("coding") → "codign"
 	 * lastTwo("cat") → "cta"
