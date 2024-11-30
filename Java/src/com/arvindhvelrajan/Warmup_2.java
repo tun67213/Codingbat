c44ba90e0f6d4e5202a294b17c58205b639d8784
tun67213 <tun67213@temple.edu>
2024-11-30 07:43:51 -0500

    Added function last2()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -93,6 +93,20 @@ public class Warmup_2
 	}
 
 	/**
+	 * Given a string, return the count of the number of times that a substring length 2
+	 * appears in the string and also as the last 2 chars of the string,
+	 * so "hixxxhi" yields 1 (we won't count the end substring).
+	 *
+	 * last2("hixxhi") → 1
+	 * last2("xaxxaxaxx") → 1
+	 * last2("axxxaaxx") → 2
+	 */
+	public int last2(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
