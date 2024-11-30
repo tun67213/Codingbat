39dcb30e917eda1d1e7753ba6ba0466c1bc866eb
tun67213 <tun67213@temple.edu>
2024-11-29 21:14:41 -0500

    Added function startOz()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -302,6 +302,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
+	 *
+	 * startOz("ozymandias") → "oz"
+	 * startOz("bzoo") → "z"
+	 * startOz("oxx") → "o"
+	 */
+	public String startOz(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
