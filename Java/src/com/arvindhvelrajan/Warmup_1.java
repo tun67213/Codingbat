a629ec89a70e8511cd22f8f182798c38d58cbed3
tun67213 <tun67213@temple.edu>
2024-11-29 20:27:19 -0500

    Added missingChar() function to be implemented

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -130,6 +130,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+	 *
+	 * missingChar("kitten", 1) → "ktten"
+	 * missingChar("kitten", 0) → "itten"
+	 * missingChar("kitten", 4) → "kittn"
+	 */
+	public String missingChar(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
