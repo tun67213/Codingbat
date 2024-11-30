dc6799887b4a641aeeb6e5a4da9d484995caa0ce
tun67213 <tun67213@temple.edu>
2024-11-29 21:40:08 -0500

    Added function everyNth()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -402,6 +402,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+	 *
+	 * everyNth("Miracle", 2) → "Mrce"
+	 * everyNth("abcdefg", 2) → "aceg"
+	 * everyNth("abcdefg", 3) → "adg"
+	 */
+	public String everyNth(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
