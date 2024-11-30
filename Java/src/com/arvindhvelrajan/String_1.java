9958ceaadb927dd538373e5173a5dbc770a6c440
tun67213 <tun67213@temple.edu>
2024-11-30 09:20:48 -0500

    Added makeTags() function

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -41,6 +41,18 @@ public class String_1
 	}
 
 	/**
+	 * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+	 *
+	 * makeTags("i", "Yay") → "<i>Yay</i>"
+	 * makeTags("i", "Hello") → "<i>Hello</i>"
+	 * makeTags("cite", "Yay") → "<cite>Yay</cite>"
+	 */
+	public String makeTags(String tag, String word)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
