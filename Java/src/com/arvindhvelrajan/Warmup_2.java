932ef3b8e7be37c9e376cbf75c008d88f664a83c
tun67213 <tun67213@temple.edu>
2024-11-30 07:39:56 -0500

    Added line feed for readability in JavaDoc for stringBits()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -68,7 +68,8 @@ public class Warmup_2
 	}
 
 	/**
-	 * Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+	 * Given a string, return a new string made of every other char starting with the first,
+	 * so "Hello" yields "Hlo".
 	 *
 	 * stringBits("Hello") → "Hlo"
 	 * stringBits("Hi") → "H"
