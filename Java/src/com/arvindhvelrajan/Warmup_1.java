e87a4a7ff9607f84a0a27b4fe5d834ea271ad070
tun67213 <tun67213@temple.edu>
2024-11-29 21:31:57 -0500

    Added function stringE()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -366,6 +366,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Return true if the given string contains between 1 and 3 'e' chars.
+	 *
+	 * stringE("Hello") → true
+	 * stringE("Heelle") → true
+	 * stringE("Heelele") → false
+	 */
+	public boolean stringE(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
