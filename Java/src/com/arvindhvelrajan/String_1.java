e8a22f097d59ba0d02627944db48dc36807b4964
tun67213 <tun67213@temple.edu>
2024-11-30 09:54:28 -0500

    Added comboString() function

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -124,6 +124,18 @@ public class String_1
 	}
 
 	/**
+	 * Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+	 *
+	 * comboString("Hello", "hi") → "hiHellohi"
+	 * comboString("hi", "Hello") → "hiHellohi"
+	 * comboString("aaa", "b") → "baaab"
+	 */
+	public String comboString(String a, String b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
