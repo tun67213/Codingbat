d8bc1bdc92b9df4e0249e2fee637f334a048b04f
tun67213 <tun67213@temple.edu>
2024-11-30 09:25:17 -0500

    Updated JavaDoc to be more readable

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -29,7 +29,8 @@ public class String_1
 	}
 
 	/**
-	 * Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+	 * Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye"
+	 * returns "HiByeByeHi".
 	 *
 	 * makeAbba("Hi", "Bye") → "HiByeByeHi"
 	 * makeAbba("Yo", "Alice") → "YoAliceAliceYo"
@@ -41,7 +42,9 @@ public class String_1
 	}
 
 	/**
-	 * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+	 * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text.
+	 * In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+	 * Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
 	 *
 	 * makeTags("i", "Yay") → "<i>Yay</i>"
 	 * makeTags("i", "Hello") → "<i>Hello</i>"
