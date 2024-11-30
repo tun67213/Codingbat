1a16b3b2e153ab83be92b4180f69d02629959b27
tun67213 <tun67213@temple.edu>
2024-11-29 21:11:42 -0500

    Added function mixStart()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -289,6 +289,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+	 *
+	 * mixStart("mix snacks") → true
+	 * mixStart("pix snacks") → true
+	 * mixStart("piz snacks") → false
+	 */
+	public boolean mixStart(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
