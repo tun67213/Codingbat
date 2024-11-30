569c7ae43cf590ab4831f2f196b3688be9287bb0
tun67213 <tun67213@temple.edu>
2024-11-29 21:04:30 -0500

    Added function loneTeen()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -263,6 +263,19 @@ public class Warmup_1
 	}
 
 	/**
+	 * We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+	 * Given 2 int values, return true if one or the other is teen, but not both.
+	 *
+	 * loneTeen(13, 99) → true
+	 * loneTeen(21, 19) → true
+	 * loneTeen(13, 13) → false
+	 */
+	public boolean loneTeen(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
