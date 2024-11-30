503a809b64ec7a7411f885657d2c79be7a3fb8a5
tun67213 <tun67213@temple.edu>
2024-11-30 17:20:02 -0500

    Added line feed to be more readable to user

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -187,7 +187,8 @@ public class String_1
 	}
 
 	/**
-	 * Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
+	 * Given a string, return a version without both the first and last char of the string.
+	 * The string may be any length, including 0.
 	 *
 	 * withouEnd2("Hello") → "ell"
 	 * withouEnd2("abc") → "b"
