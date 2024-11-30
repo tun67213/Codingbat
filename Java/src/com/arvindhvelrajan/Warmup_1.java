c9ab5b2b960582780d4fe4f27ba372dfdd673996
tun67213 <tun67213@temple.edu>
2024-11-29 21:34:44 -0500

    Added function lastDigit()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -378,6 +378,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+	 *
+	 * lastDigit(7, 17) → true
+	 * lastDigit(6, 17) → false
+	 * lastDigit(3, 113) → true
+	 */
+	public boolean lastDigit(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
