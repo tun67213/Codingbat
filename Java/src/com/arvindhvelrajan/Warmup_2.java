841141211f0dc0db9a622788042fc4b69c97758e
tun67213 <tun67213@temple.edu>
2024-11-30 07:26:44 -0500

    Added function frontTimes()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -30,6 +30,18 @@ public class Warmup_2
 	}
 
 	/**
+	 * Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+	 *
+	 * frontTimes("Chocolate", 2) → "ChoCho"
+	 * frontTimes("Chocolate", 3) → "ChoChoCho"
+	 * frontTimes("Abc", 3) → "AbcAbcAbc"
+	 */
+	public String frontTimes(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
