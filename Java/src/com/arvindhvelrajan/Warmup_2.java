0427fce97e7845a5fd09793e2e9d016266c387e8
tun67213 <tun67213@temple.edu>
2024-11-30 07:50:34 -0500

    Added function arrayFront9()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -119,6 +119,18 @@ public class Warmup_2
 	}
 
 	/**
+	 * Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+	 *
+	 * arrayFront9([1, 2, 9, 3, 4]) → true
+	 * arrayFront9([1, 2, 3, 4, 9]) → false
+	 * arrayFront9([1, 2, 3, 4, 5]) → false
+	 */
+	public boolean arrayFront9(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
