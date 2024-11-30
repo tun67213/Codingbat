e7d6295b98395cae6c3a205118eba78b69c7c7f1
tun67213 <tun67213@temple.edu>
2024-11-30 16:22:18 -0500

    Added function nonStart() to implement later

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -136,6 +136,18 @@ public class String_1
 	}
 
 	/**
+	 * Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+	 *
+	 * nonStart("Hello", "There") → "ellohere"
+	 * nonStart("java", "code") → "avaode"
+	 * nonStart("shotl", "java") → "hotlava"
+	 */
+	public String nonStart(String a, String b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
