e4fdc9adc1b5080fc5dc710df413611fa121337c
tun67213 <tun67213@temple.edu>
2024-11-30 16:29:16 -0500

    Added function right2()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -161,6 +161,18 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
+	 *
+	 * right2("Hello") → "loHel"
+	 * right2("java") → "vaja"
+	 * right2("Hi") → "Hi"
+	 */
+	public String right2(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
