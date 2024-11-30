ad5ba1e6c4741fd719b679b93e4f12bd1ba3ef67
tun67213 <tun67213@temple.edu>
2024-11-29 20:50:04 -0500

    Added function front22()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -200,6 +200,20 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given a string, take the first 2 chars and return the string with the 2 chars added at both
+	 * the front and back, so "kitten" yields"kikittenki".
+	 * If the string length is less than 2, use whatever chars are there.
+	 *
+	 * front22("kitten") → "kikittenki"
+	 * front22("Ha") → "HaHaHa"
+	 * front22("abc") → "ababcab"
+	 */
+	public String front22(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
