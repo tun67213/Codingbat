317d3b09bd82018eb7ec5e94c23a04ebae861cb1
tun67213 <tun67213@temple.edu>
2024-11-30 08:45:19 -0500

    Added test cases for helloName()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -34,5 +34,16 @@ public class String_1
 	public void main(String[] args)
 	{
 		welcome();
+		printFunction("helloName");
+		System.out.println("helloName(\"Bob\") --> \"" + helloName("Bob") + "\"");
+		System.out.println("helloName(\"Alice\") --> \"" + helloName("Alice") + "\"");
+		System.out.println("helloName(\"X\") --> \"" + helloName("X") + "\"");
+		System.out.println("helloName(\"Dolly\") --> \"" + helloName("Dolly") + "\"");
+		System.out.println("helloName(\"Alpha\") --> \"" + helloName("Alpha") + "\"");
+		System.out.println("helloName(\"Omega\") --> \"" + helloName("Omega") + "\"");
+		System.out.println("helloName(\"Goodbye\") --> \"" + helloName("Goodbye") + "\"");
+		System.out.println("helloName(\"ho ho ho\") --> \"" + helloName("ho ho ho") + "\"");
+		System.out.println("helloName(\"xyz!\") --> \"" + helloName("xyz!") + "\"");
+		System.out.println("helloName(\"Hello\") --> \"" + helloName("Hello") + "\"");
 	}
 }
