d33355f029224fcbfbaff226bbc5a8c06c3a4082
tun67213 <tun67213@temple.edu>
2024-11-30 07:29:43 -0500

    Added function countXX()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -42,6 +42,18 @@ public class Warmup_2
 	}
 
 	/**
+	 * Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+	 *
+	 * countXX("abcxx") → 1
+	 * countXX("xxx") → 2
+	 * countXX("xxxx") → 3
+	 */
+	public int countXX(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
