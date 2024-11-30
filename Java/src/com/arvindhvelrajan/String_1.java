1447c88096f2d13e91812c5c4806d82f4061eb76
tun67213 <tun67213@temple.edu>
2024-11-30 18:26:23 -0500

    Added function hasBad()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -265,6 +265,20 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return true if "bad" appears starting at index 0 or 1 in the string,
+	 * such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0.
+	 * Note: use .equals() to compare 2 strings.
+	 *
+	 * hasBad("badxx") → true
+	 * hasBad("xbadxx") → true
+	 * hasBad("xxbadxx") → false
+	 */
+	public boolean hasBad(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
