9b1563e960d67fdceb1e41be7a73bc14deabf85e
tun67213 <tun67213@temple.edu>
2024-11-29 21:21:04 -0500

    Added function close10() to be implemented later

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -326,6 +326,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
+	 *
+	 * close10(8, 13) → 8
+	 * close10(13, 8) → 8
+	 * close10(13, 7) → 0
+	 */
+	public int close10(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
