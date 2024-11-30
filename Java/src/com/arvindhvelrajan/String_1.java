60036d68eedfdf7b85bf4ec03e1350c877d3f066
tun67213 <tun67213@temple.edu>
2024-11-30 16:26:26 -0500

    Added function left2()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -148,6 +148,18 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+	 *
+	 * left2("Hello") → "lloHe"
+	 * left2("java") → "vaja"
+	 * left2("Hi") → "Hi"
+	 */
+	public String left2(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
