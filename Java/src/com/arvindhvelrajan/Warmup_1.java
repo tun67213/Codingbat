cf0afe9b51d75c200c6a4c041857750f55ce207f
tun67213 <tun67213@temple.edu>
2024-11-29 20:24:01 -0500

    Added function 'notString()'

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -118,6 +118,18 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged. Note: use .equals() to compare 2 strings.
+	 *
+	 * notString("candy") → "not candy"
+	 * notString("x") → "not x"
+	 * notString("not bad") → "not bad"
+	 */
+	public String notString(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
