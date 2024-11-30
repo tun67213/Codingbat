e5f8a9e58ab933220eac74ffa395fe0f023099b8
tun67213 <tun67213@temple.edu>
2024-11-30 16:28:55 -0500

    Split JavaDoc for left2() to be readable without scrolling

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -148,7 +148,8 @@ public class String_1
 	}
 
 	/**
-	 * Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+	 * Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end.
+	 * The string length will be at least 2.
 	 *
 	 * left2("Hello") → "lloHe"
 	 * left2("java") → "vaja"
