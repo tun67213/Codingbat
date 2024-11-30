94081059c07e6610fd7b87be527fbd166b0a3607
tun67213 <tun67213@temple.edu>
2024-11-30 09:31:11 -0500

    Added function extraEnd()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -71,6 +71,19 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+	 * The string length will be at least 2.
+	 *
+	 * extraEnd("Hello") → "lololo"
+	 * extraEnd("ab") → "ababab"
+	 * extraEnd("Hi") → "HiHiHi"
+	 */
+	public String extraEnd(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
