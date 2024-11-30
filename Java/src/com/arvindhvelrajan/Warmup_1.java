472c4289633a893d2ba8f41d2185da9c8a0c4ef3
tun67213 <tun67213@temple.edu>
2024-11-29 21:28:36 -0500

    Added function max1020()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -353,6 +353,19 @@ public class Warmup_1
 	}
 
 	/**
+	 * Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
+	 * or return 0 if neither is in that range.
+	 *
+	 * max1020(11, 19) → 19
+	 * max1020(19, 11) → 19
+	 * max1020(11, 9) → 11
+	 */
+	public int max1020(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
