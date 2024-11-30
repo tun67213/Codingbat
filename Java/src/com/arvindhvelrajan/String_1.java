c6a7175313881ec1e4778003fd1e489ff24c8fd9
tun67213 <tun67213@temple.edu>
2024-11-30 09:37:51 -0500

    Added function firstTwo()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -84,6 +84,21 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+	 * If the string is shorter than length 2, return whatever there is, so "X" yields "X",
+	 * and the empty string "" yields the empty string "".
+	 * Note that str.length() returns the length of a string.
+	 *
+	 * firstTwo("Hello") → "He"
+	 * firstTwo("abcdefg") → "ab"
+	 * firstTwo("ab") → "ab"
+	 */
+	public String firstTwo(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
