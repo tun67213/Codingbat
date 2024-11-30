4b4e9674cc2626988ef69f752086ac54c6527ce5
tun67213 <tun67213@temple.edu>
2024-11-30 07:36:52 -0500

    Added function stringBits()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -68,6 +68,18 @@ public class Warmup_2
 	}
 
 	/**
+	 * Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+	 *
+	 * stringBits("Hello") → "Hlo"
+	 * stringBits("Hi") → "H"
+	 * stringBits("Heeololeo") → "Hello"
+	 */
+	public String stringBits(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
