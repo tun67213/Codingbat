9b8f61308dde76296ee41dc7e35b48e1893e0e08
tun67213 <tun67213@temple.edu>
2024-11-30 20:59:20 -0500

    Added function withoutX2()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -445,6 +445,19 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars,
+	 * and otherwise return the string unchanged. This is a little harder than it looks.
+	 *
+	 * withoutX2("xHi") → "Hi"
+	 * withoutX2("Hxi") → "Hi"
+	 * withoutX2("Hi") → "Hi"
+	 */
+	public String withoutX2(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
