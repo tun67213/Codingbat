5af0e61b8ae2fdb804422a33b0b6797a775304b6
tun67213 <tun67213@temple.edu>
2024-11-30 19:47:29 -0500

    Added function extraFront()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -374,6 +374,19 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return a new string made of 3 copies of the first 2 chars of the original string.
+	 * The string may be any length. If there are fewer than 2 chars, use whatever is there.
+	 *
+	 * extraFront("Hello") → "HeHeHe"
+	 * extraFront("ab") → "ababab"
+	 * extraFront("H") → "HHH"
+	 */
+	public String extraFront(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
