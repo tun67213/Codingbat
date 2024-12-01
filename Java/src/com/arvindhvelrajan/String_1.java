92443e861d170ba8bd791c7396a3f0cca492c503
tun67213 <tun67213@temple.edu>
2024-11-30 20:09:35 -0500

    Added function withoutX()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -432,6 +432,19 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, if the first or last chars are 'x', return the string without those 'x' chars,
+	 * and otherwise return the string unchanged.
+	 *
+	 * withoutX("xHix") → "Hi"
+	 * withoutX("xHi") → "Hi"
+	 * withoutX("Hxix") → "Hxi"
+	 */
+	public String withoutX(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
