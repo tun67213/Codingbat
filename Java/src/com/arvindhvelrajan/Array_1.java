269da1f732d5562e8dd768b1098a7732de4f4090
tun67213 <tun67213@temple.edu>
2024-11-30 21:40:29 -0500

    Added function sameFirstLast()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -30,6 +30,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints, return true if the array is length 1 or more,
+	 * and the first element and the last element are equal.
+	 *
+	 * sameFirstLast([1, 2, 3]) → false
+	 * sameFirstLast([1, 2, 3, 1]) → true
+	 * sameFirstLast([1, 2, 1]) → true
+	 */
+	public boolean sameFirstLast(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
