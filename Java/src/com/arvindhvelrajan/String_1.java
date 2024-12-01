9404eedc9e7ee8573dfff72856523d836c595c63
tun67213 <tun67213@temple.edu>
2024-11-30 20:00:11 -0500

    Added function startWord()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -416,6 +416,22 @@ public class String_1
 	}
 
 	/**
+	 * Given a string and a second "word" string, we'll say that the word matches the string if
+	 * it appears at the front of the string, except its first char does not need to match exactly.
+	 * On a match, return the front of the string, or otherwise return the empty string.
+	 * So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip".
+	 * The word will be at least length 1.
+	 *
+	 * startWord("hippo", "hi") → "hi"
+	 * startWord("hippo", "xip") → "hip"
+	 * startWord("hippo", "i") → "h"
+	 */
+	public String startWord(String str, String word)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
