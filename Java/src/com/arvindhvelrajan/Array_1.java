35d328a99259d2dc3ac8b623f7f548b7db46bb34
tun67213 <tun67213@temple.edu>
2024-11-30 21:07:41 -0500

    Added function firstLast6()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -17,6 +17,18 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+	 *
+	 * firstLast6([1, 2, 6]) → true
+	 * firstLast6([6, 1, 2, 3]) → true
+	 * firstLast6([13, 6, 1, 2, 3]) → false
+	 */
+	public boolean firstLast6(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
