17030a3acefa742becb11d9055b572b73450a172
tun67213 <tun67213@temple.edu>
2024-11-30 19:53:59 -0500

    Added function deFront()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -402,6 +402,20 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, return a version without the first 2 chars.
+	 * Except keep the first char if it is 'a' and keep the second char if it is 'b'.
+	 * The string may be any length. Harder than it looks.
+	 *
+	 * deFront("Hello") → "llo"
+	 * deFront("java") → "va"
+	 * deFront("away") → "aay"
+	 */
+	public String deFront(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
