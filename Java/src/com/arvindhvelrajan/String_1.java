dbeac90db4898889efd18011cacc75e274e90025
tun67213 <tun67213@temple.edu>
2024-11-30 19:50:12 -0500

    Added function without2()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -387,6 +387,21 @@ public class String_1
 	}
 
 	/**
+	 * Given a string, if a length 2 substring appears at both its beginning and end,
+	 * return a string without the substring at the beginning, so "HelloHe" yields "lloHe".
+	 * The substring may overlap with itself, so "Hi" yields "".
+	 * Otherwise, return the original string unchanged.
+	 *
+	 * without2("HelloHe") → "lloHe"
+	 * without2("HelloHi") → "HelloHi"
+	 * without2("Hi") → ""
+	 */
+	public String without2(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
