57b2f3ad6dde23fee08f2a82512d56e75dd61e00
tun67213 <tun67213@temple.edu>
2024-11-30 19:43:45 -0500

    Added function minCat()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -359,6 +359,21 @@ public class String_1
 	}
 
 	/**
+	 * Given two strings, append them together (known as "concatenation") and return the result.
+	 * However, if the strings are different lengths, omit chars from the longer string so it is
+	 * the same length as the shorter string. So "Hello" and "Hi" yield "loHi".
+	 * The strings may be any length.
+	 *
+	 * minCat("Hello", "Hi") → "loHi"
+	 * minCat("Hello", "java") → "ellojava"
+	 * minCat("java", "Hello") → "javaello"
+	 */
+	public String minCat(String a, String b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
