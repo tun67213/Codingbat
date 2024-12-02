512b77f81ee970465b5e8370c4d5c01efe791285
tun67213 <tun67213@temple.edu>
2024-12-02 08:27:29 -0500

    Added function sum2()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -118,6 +118,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints, return the sum of the first 2 elements in the array.
+	 * If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+	 *
+	 * sum2([1, 2, 3]) → 3
+	 * sum2([1, 1]) → 2
+	 * sum2([1, 1, 1, 1]) → 2
+	 */
+	public int sum2(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
