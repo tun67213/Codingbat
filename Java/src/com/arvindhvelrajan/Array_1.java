d99307bf8129101b78b8ea4977f3561444a790b3
tun67213 <tun67213@temple.edu>
2024-12-02 14:56:56 -0500

    Added function start1()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -219,6 +219,18 @@ public class Array_1
 	}
 
 	/**
+	 * Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+	 *
+	 * start1([1, 2, 3], [1, 3]) → 2
+	 * start1([7, 2, 3], [1]) → 1
+	 * start1([1, 2], []) → 1
+	 */
+	public int start1(int[] a, int[] b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
