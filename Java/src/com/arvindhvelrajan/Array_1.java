0ce1d9a901aeb6cff0a08a3d05c9c9a80888f1a3
tun67213 <tun67213@temple.edu>
2024-12-02 14:40:21 -0500

    Added function no23()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -168,6 +168,18 @@ public class Array_1
 	}
 
 	/**
+	 * Given an int array length 2, return true if it does not contain a 2 or 3.
+	 *
+	 * no23([4, 5]) → true
+	 * no23([4, 2]) → false
+	 * no23([3, 5]) → false
+	 */
+	public boolean no23(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
