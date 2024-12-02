a73de2c293b695faac4d6eb7244126ec136c7d1d
tun67213 <tun67213@temple.edu>
2024-12-02 14:48:44 -0500

    Added function fix23()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -207,6 +207,18 @@ public class Array_1
 	}
 
 	/**
+	 * Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. Return the changed array.
+	 *
+	 * fix23([1, 2, 3]) → [1, 2, 0]
+	 * fix23([2, 3, 5]) → [2, 0, 5]
+	 * fix23([1, 2, 1]) → [1, 2, 1]
+	 */
+	public int[] fix23(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
