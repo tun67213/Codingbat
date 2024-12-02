c5e8944b6ba994b34e6277d74d16c43da62b437e
tun67213 <tun67213@temple.edu>
2024-12-02 08:09:57 -0500

    Added function maxEnd3()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -105,6 +105,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints length 3, figure out which is larger, the first or last element in the array,
+	 * and set all the other elements to be that value. Return the changed array.
+	 *
+	 * maxEnd3([1, 2, 3]) → [3, 3, 3]
+	 * maxEnd3([11, 5, 9]) → [11, 11, 11]
+	 * maxEnd3([2, 11, 3]) → [3, 3, 3]
+	 */
+	public int[] maxEnd3(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
