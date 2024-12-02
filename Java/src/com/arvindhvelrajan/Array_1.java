d1e31c9f2ebe22ad30403abbcb99e1c892bb4a7f
tun67213 <tun67213@temple.edu>
2024-12-02 14:37:00 -0500

    Added function has23()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -156,6 +156,18 @@ public class Array_1
 	}
 
 	/**
+	 * Given an int array length 2, return true if it contains a 2 or a 3.
+	 *
+	 * has23([2, 5]) → true
+	 * has23([4, 3]) → true
+	 * has23([4, 5]) → false
+	 */
+	public boolean has23(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
