200cb67af58a383d6f17d115c9e2c59fe80ddbc7
tun67213 <tun67213@temple.edu>
2024-12-02 08:04:07 -0500

    Added function rotateLeft3()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -80,6 +80,18 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+	 *
+	 * rotateLeft3([1, 2, 3]) → [2, 3, 1]
+	 * rotateLeft3([5, 11, 9]) → [11, 9, 5]
+	 * rotateLeft3([7, 0, 0]) → [0, 0, 7]
+	 */
+	public int[] rotateLeft3(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
