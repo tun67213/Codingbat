b8e53a6622bb8eae290fa918c668b7e932e23544
tun67213 <tun67213@temple.edu>
2024-12-01 19:58:27 -0500

    Added function commonEnd()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -55,6 +55,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given 2 arrays of ints, a and b, return true if they have the same first element
+	 * or they have the same last element. Both arrays will be length 1 or more.
+	 *
+	 * commonEnd([1, 2, 3], [7, 3]) → true
+	 * commonEnd([1, 2, 3], [7, 3, 2]) → false
+	 * commonEnd([1, 2, 3], [1, 3]) → true
+	 */
+	public boolean commonEnd(int[] a, int[] b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
