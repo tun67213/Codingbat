3dc3436a2234f873b8e1c1609f630b1678d92705
tun67213 <tun67213@temple.edu>
2024-12-02 20:34:48 -0500

    Added function front11()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -349,6 +349,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given 2 int arrays, a and b, of any length, return a new array with the first element of each array.
+	 * If either array is length 0, ignore that array.
+	 *
+	 * front11([1, 2, 3], [7, 9, 8]) → [1, 7]
+	 * front11([1], [2]) → [1, 2]
+	 * front11([1, 7], []) → [1]
+	 */
+	public int[] front11(int[] a, int[] b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
