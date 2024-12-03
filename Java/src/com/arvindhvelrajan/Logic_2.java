dfb7a0d6924d03938aeaec685d3f57d2ebe602f3
tun67213 <tun67213@temple.edu>
2024-12-03 08:30:56 -0500

    Added function evenlySpaced()

--- Java/src/com/arvindhvelrajan/Logic_2.java
+++ Java/src/com/arvindhvelrajan/Logic_2.java
@@ -116,6 +116,20 @@ public class Logic_2
 	}
 
 	/**
+	 * Given three ints, a b c, one of them is small, one is medium and one is large.
+	 * Return true if the three values are evenly spaced, so the difference between small and medium is the same
+	 * as the difference between medium and large.
+	 *
+	 * evenlySpaced(2, 4, 6) → true
+	 * evenlySpaced(4, 6, 2) → true
+	 * evenlySpaced(4, 6, 3) → false
+	 */
+	public boolean evenlySpaced(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
