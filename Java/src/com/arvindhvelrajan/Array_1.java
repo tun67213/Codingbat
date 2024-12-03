5fcdd0ec3fa6bef2173524243dc257f78992867f
tun67213 <tun67213@temple.edu>
2024-12-02 20:10:33 -0500

    Updated JavaDoc for swapEnds()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -269,7 +269,8 @@ public class Array_1
 	}
 
 	/**
-	 * Given an array of ints, swap the first and last elements in the array. Return the modified array. The array length will be at least 1.
+	 * Given an array of ints, swap the first and last elements in the array. Return the modified array.
+	 * The array length will be at least 1.
 	 *
 	 * swapEnds([1, 2, 3, 4]) → [4, 2, 3, 1]
 	 * swapEnds([1, 2, 3]) → [3, 2, 1]
