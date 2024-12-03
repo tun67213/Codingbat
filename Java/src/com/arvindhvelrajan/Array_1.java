e553f126c6b7be57d726e836fc332dec54675cd5
tun67213 <tun67213@temple.edu>
2024-12-02 20:13:11 -0500

    Added function maxTriple()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -295,6 +295,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints of odd length, look at the first, last, and middle values in the array and
+	 * return the largest. The array length will be a least 1.
+	 *
+	 * maxTriple([1, 2, 3]) → 3
+	 * maxTriple([1, 5, 3]) → 5
+	 * maxTriple([5, 2, 3]) → 5
+	 */
+	public int maxTriple(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
