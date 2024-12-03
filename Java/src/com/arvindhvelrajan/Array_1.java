90f62f919344658a0b967133cd7c29c198aab7b5
tun67213 <tun67213@temple.edu>
2024-12-02 20:21:39 -0500

    Added function unlucky1()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -321,6 +321,19 @@ public class Array_1
 	}
 
 	/**
+	 * We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1.
+	 * Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+	 *
+	 * unlucky1([1, 3, 4, 5]) → true
+	 * unlucky1([2, 1, 3, 4, 5]) → true
+	 * unlucky1([1, 1, 1]) → false
+	 */
+	public boolean unlucky1(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
