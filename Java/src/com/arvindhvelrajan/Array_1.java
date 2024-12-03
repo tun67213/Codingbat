3eb48acb86c5afa2ae83f2d217e55d73bd77d984
tun67213 <tun67213@temple.edu>
2024-12-02 20:05:51 -0500

    Added function plusTwo()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -257,6 +257,18 @@ public class Array_1
 	}
 
 	/**
+	 * Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+	 *
+	 * plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
+	 * plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
+	 * plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]
+	 */
+	public int[] plusTwo(int[] a, int[] b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
