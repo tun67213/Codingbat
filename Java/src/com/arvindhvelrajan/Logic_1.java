824984c505a1b0bee17c18f54d8f138c1043b257
tun67213 <tun67213@temple.edu>
2024-12-02 21:03:44 -0500

    Added function sortaSum()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -83,6 +83,19 @@ public class Logic_1
 	}
 
 	/**
+	 * Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden,
+	 * so in that case just return 20.
+	 *
+	 * sortaSum(3, 4) → 7
+	 * sortaSum(9, 4) → 20
+	 * sortaSum(10, 11) → 21
+	 */
+	public int sortaSum(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
