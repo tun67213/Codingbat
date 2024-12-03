c55796258433fb88961be4b59b7b25b03b3c4f8b
tun67213 <tun67213@temple.edu>
2024-12-02 22:50:28 -0500

    Added function alarmClock()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -96,6 +96,21 @@ public class Logic_1
 	}
 
 	/**
+	 * Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we
+	 * are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring.
+	 * Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00".
+	 * Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+	 *
+	 * alarmClock(1, false) → "7:00"
+	 * alarmClock(5, false) → "7:00"
+	 * alarmClock(0, false) → "10:00"
+	 */
+	public String alarmClock(int day, boolean vacation)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
