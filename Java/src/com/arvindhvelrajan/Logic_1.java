b10f159e004e4537ddcee69e575f7a5c23cbe731
tun67213 <tun67213@temple.edu>
2024-12-03 00:39:21 -0500

    Added function sumLimit()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -430,6 +430,21 @@ public class Logic_1
 	}
 
 	/**
+	 * Given 2 non-negative ints, a and b, return their sum, so long as the sum has the same number of digits as a.
+	 * If the sum has more digits than a, just return a without b.
+	 * (Note: one way to compute the number of digits of a non-negative int n is to convert it to
+	 * a string with String.valueOf(n) and then check the length of the string.)
+	 *
+	 * sumLimit(2, 3) → 5
+	 * sumLimit(8, 3) → 8
+	 * sumLimit(8, 1) → 9
+	 */
+	public int sumLimit(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
