47fd1764b99f34a178811691936216d8178860cd
tun67213 <tun67213@temple.edu>
2024-12-03 00:14:26 -0500

    Added function lessBy10()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -329,6 +329,18 @@ public class Logic_1
 	}
 
 	/**
+	 * Given three ints, a b c, return true if one of them is 10 or more less than one of the others.
+	 *
+	 * lessBy10(1, 7, 11) → true
+	 * lessBy10(1, 7, 10) → false
+	 * lessBy10(11, 1, 7) → true
+	 */
+	public boolean lessBy10(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
