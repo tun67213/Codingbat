b02505dbd193863de866d9533ffa6f90dfa1cef4
tun67213 <tun67213@temple.edu>
2024-12-02 23:00:01 -0500

    Added function in1To10()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -125,6 +125,20 @@ public class Logic_1
 	}
 
 	/**
+	 * Given a number n, return true if n is in the range 1..10, inclusive.
+	 * Unless outsideMode is true, in which case return true if the number is less or equal to 1,
+	 * or greater or equal to 10.
+	 *
+	 * in1To10(5, false) → true
+	 * in1To10(11, false) → false
+	 * in1To10(11, true) → true
+	 */
+	public boolean in1To10(int n, boolean outsideMode)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
