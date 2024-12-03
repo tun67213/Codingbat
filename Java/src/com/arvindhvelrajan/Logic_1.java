910f2b0771088ef9eb3adf00294625e9b7baa1b3
tun67213 <tun67213@temple.edu>
2024-12-02 23:16:50 -0500

    Added function less20()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -179,6 +179,19 @@ public class Logic_1
 	}
 
 	/**
+	 * Return true if the given non-negative number is 1 or 2 less than a multiple of 20.
+	 * So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+	 *
+	 * less20(18) → true
+	 * less20(19) → true
+	 * less20(20) → false
+	 */
+	public boolean less20(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
