b6682a85a3d4de5715f80b9568a30028ab5ce483
tun67213 <tun67213@temple.edu>
2024-12-02 20:10:59 -0500

    Added function midThree()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -282,6 +282,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints of odd length, return a new array length 3 containing the elements from
+	 * the middle of the array. The array length will be at least 3.
+	 *
+	 * midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
+	 * midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
+	 * midThree([1, 2, 3]) → [1, 2, 3]
+	 */
+	public int[] midThree(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
