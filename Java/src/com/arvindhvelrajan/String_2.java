3e5636f6db01152042d99b919c92aeedca1b455f
tun67213 <tun67213@temple.edu>
2024-12-03 16:03:32 -0500

    Added function repeatFront()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -145,6 +145,20 @@ public class String_2
 	}
 
 	/**
+	 * Given a string and an int n, return a string made of the first n characters of the string,
+	 * followed by the first n-1 characters of the string, and so on. You may assume that n is
+	 * between 0 and the length of the string, inclusive (i.e. n >= 0 and n <= str.length()).
+	 *
+	 * repeatFront("Chocolate", 4) → "ChocChoChC"
+	 * repeatFront("Chocolate", 3) → "ChoChC"
+	 * repeatFront("Ice Cream", 2) → "IcI"
+	 */
+	public String repeatFront(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
