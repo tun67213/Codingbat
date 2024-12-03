715164d383761d6690925793642b18bf469250dd
tun67213 <tun67213@temple.edu>
2024-12-02 23:45:24 -0500

    Added function fizzString()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -250,6 +250,21 @@ public class Logic_1
 	}
 
 	/**
+	 * Given a string str, if the string starts with "f" return "Fizz".
+	 * If the string ends with "b" return "Buzz".
+	 * If both the "f" and "b" conditions are true, return "FizzBuzz".
+	 * In all other cases, return the string unchanged. (See also: FizzBuzz Code)
+	 *
+	 * fizzString("fig") → "Fizz"
+	 * fizzString("dib") → "Buzz"
+	 * fizzString("fib") → "FizzBuzz"
+	 */
+	public String fizzString(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
