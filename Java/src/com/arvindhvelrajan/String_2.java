9cd1776782196911c3420ca58b4fb2ca8ec5b9ef
tun67213 <tun67213@temple.edu>
2024-12-03 16:40:11 -0500

    Added function repeatSeparator()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -159,6 +159,19 @@ public class String_2
 	}
 
 	/**
+	 * Given two strings, word and a separator sep, return a big string made of count occurrences of the word,
+	 * separated by the separator string.
+	 *
+	 * repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+	 * repeatSeparator("This", "And", 2) → "ThisAndThis"
+	 * repeatSeparator("This", "And", 1) → "This"
+	 */
+	public String repeatSeparator(String word, String sep, int count)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
