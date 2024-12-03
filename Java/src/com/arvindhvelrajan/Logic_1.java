7ac78cb33f583500a7f738b02a1acf8ef2c0c432
tun67213 <tun67213@temple.edu>
2024-12-03 00:10:22 -0500

    Added function lastDigit()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -316,6 +316,19 @@ public class Logic_1
 	}
 
 	/**
+	 * Given three ints, a b c, return true if two or more of them have the same rightmost digit.
+	 * The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.
+	 *
+	 * lastDigit(23, 19, 13) → true
+	 * lastDigit(23, 19, 12) → false
+	 * lastDigit(23, 19, 3) → true
+	 */
+	public boolean lastDigit(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
