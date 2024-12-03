76551dcc432917810aed61f7bef018d367aad475
tun67213 <tun67213@temple.edu>
2024-12-03 08:30:17 -0500

    Updated JavaDoc for blackjack() to be in 2 lines

--- Java/src/com/arvindhvelrajan/Logic_2.java
+++ Java/src/com/arvindhvelrajan/Logic_2.java
@@ -103,7 +103,8 @@ public class Logic_2
 	}
 
 	/**
-	 * Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. Return 0 if they both go over.
+	 * Given 2 int values greater than 0, return whichever value is nearest to 21 without going over.
+	 * Return 0 if they both go over.
 	 *
 	 * blackjack(19, 21) → 21
 	 * blackjack(21, 19) → 21
