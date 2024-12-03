ca24ab9a7ea51880c53ede0681f858ed7771dca7
tun67213 <tun67213@temple.edu>
2024-12-03 08:17:28 -0500

    Added function roundSum()

--- Java/src/com/arvindhvelrajan/Logic_2.java
+++ Java/src/com/arvindhvelrajan/Logic_2.java
@@ -72,6 +72,23 @@ public class Logic_2
 	}
 
 	/**
+	 * For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is
+	 * 5 or more, so 15 rounds up to 20.
+	 * Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5,
+	 * so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values.
+	 * To avoid code repetition, write a separate helper "public int round10(int num) {" and call it 3 times.
+	 * Write the helper entirely below and at the same indent level as roundSum().
+	 *
+	 * roundSum(16, 17, 18) → 60
+	 * roundSum(12, 13, 14) → 30
+	 * roundSum(6, 4, 4) → 10
+	 */
+	public int roundSum(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
