df2f99cce379d88bde3801febf0615f71c55192a
tun67213 <tun67213@temple.edu>
2024-12-02 20:43:29 -0500

    Added function cigarParty()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -17,6 +17,21 @@ public class Logic_1
 	}
 
 	/**
+	 * When squirrels get together for a party, they like to have cigars.
+	 * A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+	 * Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+	 * Return true if the party with the given values is successful, or false otherwise.
+	 *
+	 * cigarParty(30, false) → false
+	 * cigarParty(50, false) → true
+	 * cigarParty(70, true) → true
+	 */
+	public boolean cigarParty(int cigars, boolean isWeekend)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
