82ea7338a78b20711a6afd477d9acee57aa6055b
tun67213 <tun67213@temple.edu>
2024-12-03 10:05:41 -0500

    Added function xyBalance()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -104,6 +104,20 @@ public class String_2
 	}
 
 	/**
+	 * We'll say that a String is xy-balanced if for all the 'x' chars in the string,
+	 * there exists a 'y' char somewhere later in the string. So "xxy" is balanced, but "xyx" is not.
+	 * One 'y' can balance multiple 'x's. Return true if the given string is xy-balanced.
+	 *
+	 * xyBalance("aaxbby") → true
+	 * xyBalance("aaxbb") → false
+	 * xyBalance("yaaxbb") → false
+	 */
+	public boolean xyBalance(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
