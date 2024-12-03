0d88c667e56eb66845a403b6a355c9c75166988d
tun67213 <tun67213@temple.edu>
2024-12-02 23:26:28 -0500

    Added function teenSum()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -205,6 +205,20 @@ public class Logic_1
 	}
 
 	/**
+	 * Given 2 ints, a and b, return their sum.
+	 * However, "teen" values in the range 13..19 inclusive, are extra lucky.
+	 * So if either value is a teen, just return 19.
+	 *
+	 * teenSum(3, 4) → 7
+	 * teenSum(10, 13) → 19
+	 * teenSum(13, 2) → 19
+	 */
+	public int teenSum(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
