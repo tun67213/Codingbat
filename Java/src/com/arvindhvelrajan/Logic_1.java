08e91b21b86fd487d2719c353812c9208b7de205
tun67213 <tun67213@temple.edu>
2024-12-02 23:56:04 -0500

    Added function twoAsOne()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -277,6 +277,18 @@ public class Logic_1
 	}
 
 	/**
+	 * Given three ints, a b c, return true if it is possible to add two of the ints to get the third.
+	 *
+	 * twoAsOne(1, 2, 3) → true
+	 * twoAsOne(3, 1, 2) → true
+	 * twoAsOne(3, 2, 2) → false
+	 */
+	public boolean twoAsOne(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
