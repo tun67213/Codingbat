5efd3a9b56519dad726eea863fb6f9272e9a3b18
tun67213 <tun67213@temple.edu>
2024-12-02 23:07:50 -0500

    Added function more20()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -153,6 +153,19 @@ public class Logic_1
 	}
 
 	/**
+	 * Return true if the given non-negative number is 1 or 2 more than a multiple of 20.
+	 * See also: Introduction to Mod
+	 *
+	 * more20(20) → false
+	 * more20(21) → true
+	 * more20(22) → true
+	 */
+	public boolean more20(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
