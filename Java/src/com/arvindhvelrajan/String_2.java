e66cf822fac06da7c2143ae057f8e34173bcc4e7
tun67213 <tun67213@temple.edu>
2024-12-03 16:00:04 -0500

    Added function repeatEnd()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -132,6 +132,19 @@ public class String_2
 	}
 
 	/**
+	 * Given a string and an int n, return a string made of n repetitions of the last n characters
+	 * of the string. You may assume that n is between 0 and the length of the string, inclusive.
+	 *
+	 * repeatEnd("Hello", 3) → "llollollo"
+	 * repeatEnd("Hello", 2) → "lolo"
+	 * repeatEnd("Hello", 1) → "o"
+	 */
+	public String repeatEnd(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
