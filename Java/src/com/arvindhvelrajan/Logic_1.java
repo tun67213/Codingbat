ee2d7e3026459cea6de07082d9289ff381b91fb3
tun67213 <tun67213@temple.edu>
2024-12-02 23:03:22 -0500

    Added function specialEleven()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -139,6 +139,20 @@ public class Logic_1
 	}
 
 	/**
+	 * We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
+	 * Return true if the given non-negative number is special.
+	 * Use the % "mod" operator -- see Introduction to Mod
+	 *
+	 * specialEleven(22) → true
+	 * specialEleven(23) → true
+	 * specialEleven(24) → false
+	 */
+	public boolean specialEleven(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
