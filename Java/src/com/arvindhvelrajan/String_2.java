0d09ca86315620aff84753405b7a4eedaa1f72f1
tun67213 <tun67213@temple.edu>
2024-12-03 09:46:08 -0500

    Added function endOther()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -66,6 +66,20 @@ public class String_2
 	}
 
 	/**
+	 * Given two strings, return true if either of the strings appears at the very end of the other string,
+	 * ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+	 * Note: str.toLowerCase() returns the lowercase version of a string.
+	 *
+	 * endOther("Hiabc", "abc") → true
+	 * endOther("AbC", "HiaBc") → true
+	 * endOther("abc", "abXabc") → true
+	 */
+	public boolean endOther(String a, String b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
