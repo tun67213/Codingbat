be5bec82972b866c5b877398309e5def8ce9b50f
tun67213 <tun67213@temple.edu>
2024-12-03 00:18:26 -0500

    Added function withoutDoubles()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -341,6 +341,20 @@ public class Logic_1
 	}
 
 	/**
+	 * Return the sum of two 6-sided dice rolls, each in the range 1..6.
+	 * However, if noDoubles is true, if the two dice show the same value,
+	 * increment one die to the next value, wrapping around to 1 if its value was 6.
+	 *
+	 * withoutDoubles(2, 3, true) → 5
+	 * withoutDoubles(3, 3, true) → 7
+	 * withoutDoubles(3, 3, false) → 6
+	 */
+	public int withoutDoubles(int die1, int die2, boolean noDoubles)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
