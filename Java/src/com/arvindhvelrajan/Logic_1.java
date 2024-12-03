471f26837792ac183c30d3e83cd33f5347572643
tun67213 <tun67213@temple.edu>
2024-12-02 23:22:09 -0500

    Add function nearTen()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -192,6 +192,19 @@ public class Logic_1
 	}
 
 	/**
+	 * Given a non-negative number "num", return true if num is within 2 of a multiple of 10.
+	 * Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+	 *
+	 * nearTen(12) → true
+	 * nearTen(17) → false
+	 * nearTen(19) → true
+	 */
+	public boolean nearTen(int num)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
