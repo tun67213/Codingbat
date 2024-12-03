177a0c2dd476dac9c40a11e9ef6af3e49fcce194
tun67213 <tun67213@temple.edu>
2024-12-03 08:06:14 -0500

    Added function luckySum()

--- Java/src/com/arvindhvelrajan/Logic_2.java
+++ Java/src/com/arvindhvelrajan/Logic_2.java
@@ -41,6 +41,19 @@ public class Logic_2
 	}
 
 	/**
+	 * Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards
+	 * the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
+	 *
+	 * luckySum(1, 2, 3) → 6
+	 * luckySum(1, 2, 13) → 3
+	 * luckySum(1, 13, 3) → 1
+	 */
+	public int luckySum(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
