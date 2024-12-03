d4afda84179369e922b1e4d3987794a8a42c32ce
tun67213 <tun67213@temple.edu>
2024-12-02 23:51:21 -0500

    Added function fizzString2()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -265,6 +265,18 @@ public class Logic_1
 	}
 
 	/**
+	 * Given an int n, return the string form of the number followed by "!". So the int 6 yields "6!".
+	 * Except if the number is divisible by 3 use "Fizz" instead of the number, and if the number is
+	 * divisible by 5 use "Buzz", and if divisible by both 3 and 5, use "FizzBuzz".
+	 * Note: the % "mod" operator computes the remainder after division, so 23 % 10 yields 3.
+	 * What will the remainder be when one number divides evenly into another?
+	 */
+	public String fizzString2(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
