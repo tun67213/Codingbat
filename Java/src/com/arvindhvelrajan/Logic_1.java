916efea45ebac74fc6f13fa290c99f533d0429d8
tun67213 <tun67213@temple.edu>
2024-12-03 00:00:32 -0500

    Added function inOrder()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -289,6 +289,19 @@ public class Logic_1
 	}
 
 	/**
+	 * Given three ints, a b c, return true if b is greater than a, and c is greater than b.
+	 * However, with the exception that if "bOk" is true, b does not need to be greater than a.
+	 *
+	 * inOrder(1, 2, 4, false) → true
+	 * inOrder(1, 2, 1, false) → false
+	 * inOrder(1, 1, 2, true) → true
+	 */
+	public boolean inOrder(int a, int b, int c, boolean bOk)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
