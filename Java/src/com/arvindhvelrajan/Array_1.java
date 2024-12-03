62efa4847fbf66f809dc26aeb252ad58b5f4118c
tun67213 <tun67213@temple.edu>
2024-12-02 20:02:31 -0500

    Added function makeMiddle()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -244,6 +244,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given an array of ints of even length, return a new array length 2 containing the middle two elements
+	 * from the original array. The original array will be length 2 or more.
+	 *
+	 * makeMiddle([1, 2, 3, 4]) → [2, 3]
+	 * makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
+	 * makeMiddle([1, 2]) → [1, 2]
+	 */
+	public int[] makeMiddle(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
