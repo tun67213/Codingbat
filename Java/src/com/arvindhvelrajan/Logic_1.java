d81ad2e7435e9aab4e56502215ea3cfe2dfc288e
tun67213 <tun67213@temple.edu>
2024-12-03 00:35:30 -0500

    Added function shareDigit()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -418,6 +418,18 @@ public class Logic_1
 	}
 
 	/**
+	 * Given two ints, each in the range 10..99, return true if there is a digit that appears in both numbers, such as the 2 in 12 and 23. (Note: division, e.g. n/10, gives the left digit while the % "mod" n%10 gives the right digit.)
+	 *
+	 * shareDigit(12, 23) → true
+	 * shareDigit(12, 43) → false
+	 * shareDigit(12, 44) → false
+	 */
+	public boolean shareDigit(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
