04cb724d87e06062045f9a5ae03d847aca289dc2
tun67213 <tun67213@temple.edu>
2024-12-02 19:58:27 -0500

    Added function biggerTwo()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -231,6 +231,19 @@ public class Array_1
 	}
 
 	/**
+	 * Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array.
+	 * Return the array which has the largest sum. In event of a tie, return a.
+	 *
+	 * biggerTwo([1, 2], [3, 4]) → [3, 4]
+	 * biggerTwo([3, 4], [1, 2]) → [3, 4]
+	 * biggerTwo([1, 1], [1, 2]) → [1, 2]
+	 */
+	public int[] biggerTwo(int[] a, int[] b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
