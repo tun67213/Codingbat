d055e80a4ca082a22990dbc1897f9b1516ab61b3
tun67213 <tun67213@temple.edu>
2024-12-03 00:25:30 -0500

    Added function redTicket()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -370,6 +370,22 @@ public class Logic_1
 	}
 
 	/**
+	 * You have a red lottery ticket showing ints a, b, and c, each of which is 0, 1, or 2.
+	 * If they are all the value 2, the result is 10.
+	 * Otherwise if they are all the same, the result is 5.
+	 * Otherwise so long as both b and c are different from a, the result is 1.
+	 * Otherwise the result is 0.
+	 *
+	 * redTicket(2, 2, 2) → 10
+	 * redTicket(2, 2, 1) → 0
+	 * redTicket(0, 0, 0) → 5
+	 */
+	public int redTicket(int a, int b, int c)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
