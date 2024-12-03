609c9554a397f8894752c79423b2dc5e24e4b610
tun67213 <tun67213@temple.edu>
2024-12-02 22:55:02 -0500

    Added function love6()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -111,6 +111,20 @@ public class Logic_1
 	}
 
 	/**
+	 * The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6.
+	 * Or if their sum or difference is 6.
+	 * Note: the function Math.abs(num) computes the absolute value of a number.
+	 *
+	 * love6(6, 4) → true
+	 * love6(4, 5) → false
+	 * love6(1, 5) → true
+	 */
+	public boolean love6(int a, int b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
