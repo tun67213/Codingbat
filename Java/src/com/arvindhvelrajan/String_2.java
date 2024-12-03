fc976bec69ef4f5040669dccdb34cbc507faa007
tun67213 <tun67213@temple.edu>
2024-12-03 08:54:11 -0500

    Added function countCode()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -53,6 +53,19 @@ public class String_2
 	}
 
 	/**
+	 * Return the number of times that the string "code" appears anywhere in the given string,
+	 * except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+	 *
+	 * countCode("aaacodebbb") → 1
+	 * countCode("codexxcode") → 2
+	 * countCode("cozexxcope") → 2
+	 */
+	public int countCode(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
