ffb7390dea943512e28e4d4239793afcfdc54f3c
tun67213 <tun67213@temple.edu>
2024-12-03 00:44:26 -0500

    Added function makeBricks()

--- Java/src/com/arvindhvelrajan/Logic_2.java
+++ Java/src/com/arvindhvelrajan/Logic_2.java
@@ -12,10 +12,22 @@ public class Logic_2
 	}
 
 	/**
+	 * We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return true if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops. See also: Introduction to MakeBricks
+	 *
+	 * makeBricks(3, 1, 8) → true
+	 * makeBricks(3, 1, 9) → false
+	 * makeBricks(3, 2, 10) → true
+	 */
+	public boolean makeBricks(int small, int big, int goal)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
 	{
-		
+
 	}
 }
