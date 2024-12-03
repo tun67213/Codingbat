d2f83660e110d779fc06cfcd708eb4d839a06590
tun67213 <tun67213@temple.edu>
2024-12-02 23:12:39 -0500

    Added function old35()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -166,6 +166,19 @@ public class Logic_1
 	}
 
 	/**
+	 * Return true if the given non-negative number is a multiple of 3 or 5, but not both.
+	 * Use the % "mod" operator -- see Introduction to Mod
+	 *
+	 * old35(3) → true
+	 * old35(10) → true
+	 * old35(15) → false
+	 */
+	public boolean old35(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
