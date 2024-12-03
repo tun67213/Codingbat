c93daadc37c4a46f047010913d2e5c1e1f38234a
tun67213 <tun67213@temple.edu>
2024-12-02 20:17:41 -0500

    Added function frontPiece()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -308,6 +308,19 @@ public class Array_1
 	}
 
 	/**
+	 * Given an int array of any length, return a new array of its first 2 elements.
+	 * If the array is smaller than length 2, use whatever elements are present.
+	 *
+	 * frontPiece([1, 2, 3]) → [1, 2]
+	 * frontPiece([1, 2]) → [1, 2]
+	 * frontPiece([1]) → [1]
+	 */
+	public int[] frontPiece(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
