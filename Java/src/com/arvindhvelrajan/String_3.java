e95b2d568c8091aa7243b18a19d9082f687367e5
tun67213 <tun67213@temple.edu>
2024-12-04 09:30:17 -0500

    Added function countYZ()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -17,6 +17,21 @@ public class String_3
 	}
 
 	/**
+	 * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and
+	 * the 'z' in "fez" count, but not the 'y' in "yellow" (not case sensitive).
+	 * We'll say that a y or z is at the end of a word if there is not an alphabetic letter immediately following it.
+	 * (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
+	 *
+	 * countYZ("fez day") → 2
+	 * countYZ("day fez") → 2
+	 * countYZ("day fyyyz") → 2
+	 */
+	public int countYZ(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
