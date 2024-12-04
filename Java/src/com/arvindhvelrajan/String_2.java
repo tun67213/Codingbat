e6296433428deeb0d160cc921519cfe699ebe6f0
tun67213 <tun67213@temple.edu>
2024-12-04 09:23:03 -0500

    Added function wordEnds()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -279,6 +279,18 @@ public class String_2
 	}
 
 	/**
+	 * Given a string and a non-empty word string, return a string made of each char just before and just after every appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
+	 *
+	 * wordEnds("abcXY123XYijk", "XY") → "c13i"
+	 * wordEnds("XY123XY", "XY") → "13"
+	 * wordEnds("XY1XY", "XY") → "11"
+	 */
+	public String wordEnds(String str, String word)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
