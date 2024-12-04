d43c6fafbc17afb7a36a8e46464a722bd3429183
tun67213 <tun67213@temple.edu>
2024-12-04 08:04:49 -0500

    Added function prefixAgain()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -172,6 +172,20 @@ public class String_2
 	}
 
 	/**
+	 * Given a string, consider the prefix string made of the first N chars of the string.
+	 * Does that prefix string appear somewhere else in the string?
+	 * Assume that the string is not empty and that N is in the range 1..str.length().
+	 *
+	 * prefixAgain("abXYabc", 1) → true
+	 * prefixAgain("abXYabc", 2) → true
+	 * prefixAgain("abXYabc", 3) → false
+	 */
+	public boolean prefixAgain(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
