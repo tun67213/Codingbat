9bbbf7981032bd499a5609d028889954ee9190ad
tun67213 <tun67213@temple.edu>
2024-12-04 08:12:01 -0500

    Added function xyzMiddle()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -186,6 +186,20 @@ public class String_2
 	}
 
 	/**
+	 * Given a string, does "xyz" appear in the middle of the string?
+	 * To define middle, we'll say that the number of chars to the left and right of the "xyz"
+	 * must differ by at most one. This problem is harder than it looks.
+	 *
+	 * xyzMiddle("AAxyzBB") → true
+	 * xyzMiddle("AxyzBB") → true
+	 * xyzMiddle("AxyzBBB") → false
+	 */
+	public boolean xyzMiddle(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
