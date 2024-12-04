af45ee29c1e7f8a75e9563f766176893f9288ec6
tun67213 <tun67213@temple.edu>
2024-12-04 09:02:45 -0500

    Added function oneTwo()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -227,6 +227,20 @@ public class String_2
 	}
 
 	/**
+	 * Given a string, compute a new string by moving the first char to come after the next two chars,
+	 * so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd".
+	 * Ignore any group of fewer than 3 chars at the end.
+	 *
+	 * oneTwo("abc") → "bca"
+	 * oneTwo("tca") → "cat"
+	 * oneTwo("tcagdo") → "catdog"
+	 */
+	public String oneTwo(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
