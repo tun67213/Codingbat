067308a6891799b79ea2093b57eaeb97a1c91070
tun67213 <tun67213@temple.edu>
2024-12-04 09:19:31 -0500

    Added function plusOut()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -266,6 +266,19 @@ public class String_2
 	}
 
 	/**
+	 * Given a string and a non-empty word string, return a version of the original String where all chars have been
+	 * replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.
+	 *
+	 * plusOut("12xy34", "xy") → "++xy++"
+	 * plusOut("12xy34", "1") → "1+++++"
+	 * plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"
+	 */
+	public String plusOut(String str, String word)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
