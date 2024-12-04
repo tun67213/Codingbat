c114cc99aec220f3edafe83dab3bd62c07aee77d
tun67213 <tun67213@temple.edu>
2024-12-04 09:35:08 -0500

    Added function withoutString()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -32,6 +32,20 @@ public class String_3
 	}
 
 	/**
+	 * Given two strings, base and remove, return a version of the base string where all instances of the remove string
+	 * have been removed (not case sensitive). You may assume that the remove string is length 1 or more.
+	 * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+	 *
+	 * withoutString("Hello there", "llo") → "He there"
+	 * withoutString("Hello there", "e") → "Hllo thr"
+	 * withoutString("Hello there", "x") → "Hello there"
+	 */
+	public String withoutString(String base, String remove)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
