0ff9cd3d6884eeeab5232b381a8e2d03dd3be1cc
tun67213 <tun67213@temple.edu>
2024-12-05 08:14:17 -0500

    Added function sum13()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -58,6 +58,19 @@ public class Array_2
 	}
 
 	/**
+	 * Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky,
+	 * so it does not count and numbers that come immediately after a 13 also do not count.
+	 *
+	 * sum13([1, 2, 2, 1]) → 6
+	 * sum13([1, 1]) → 2
+	 * sum13([1, 2, 2, 1, 13]) → 6
+	 */
+	public int sum13(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
