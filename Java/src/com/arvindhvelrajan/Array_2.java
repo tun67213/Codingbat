ca14326692fc350d57048d4a2ac3fe8a6e0364d3
tun67213 <tun67213@temple.edu>
2024-12-05 08:32:23 -0500

    Added function sum28()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -108,6 +108,18 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
+	 *
+	 * sum28([2, 3, 2, 2, 4, 2]) → true
+	 * sum28([2, 3, 2, 2, 4, 2, 2]) → false
+	 * sum28([1, 2, 3, 4]) → false
+	 */
+	public boolean sum28(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
