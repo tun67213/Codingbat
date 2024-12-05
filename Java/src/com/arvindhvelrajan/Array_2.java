aabb2366552c64439eabdb0a2d1ab51983e3224f
tun67213 <tun67213@temple.edu>
2024-12-05 09:56:47 -0500

    Added function no14()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -195,6 +195,18 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if it contains no 1's or it contains no 4's.
+	 *
+	 * no14([1, 2, 3]) → true
+	 * no14([1, 2, 3, 4]) → false
+	 * no14([2, 3, 4]) → true
+	 */
+	public boolean no14(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
