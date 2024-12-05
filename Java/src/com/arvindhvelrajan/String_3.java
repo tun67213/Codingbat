62df006da484f2bd30be3c9e49afa09859df1c88
tun67213 <tun67213@temple.edu>
2024-12-04 19:10:08 -0500

    Split JavaDoc for sameEnds() into two lines

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -100,7 +100,8 @@ public class String_3
 	}
 
 	/**
-	 * Given a string, return the longest substring that appears at both the beginning and end of the string without overlapping. For example, sameEnds("abXab") is "ab".
+	 * Given a string, return the longest substring that appears at both the beginning and end of the string without
+	 * overlapping. For example, sameEnds("abXab") is "ab".
 	 *
 	 * sameEnds("abXYab") → "ab"
 	 * sameEnds("xx") → "x"
