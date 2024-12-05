56c306aa8bcd4178b00be92103bbcc9a9d20d3a0
tun67213 <tun67213@temple.edu>
2024-12-05 10:07:40 -0500

    Added function either24()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -220,6 +220,18 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
+	 *
+	 * either24([1, 2, 2]) → true
+	 * either24([4, 4, 1]) → true
+	 * either24([4, 4, 1, 2, 2]) → false
+	 */
+	public boolean either24(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
