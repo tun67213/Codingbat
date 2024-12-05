af88ab008bb351c5cadab8aa934d20e9e81c15e2
tun67213 <tun67213@temple.edu>
2024-12-04 19:00:28 -0500

    Added function countTriple()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -72,6 +72,19 @@ public class String_3
 	}
 
 	/**
+	 * We'll say that a "triple" in a string is a char appearing three times in a row.
+	 * Return the number of triples in the given string. The triples may overlap.
+	 *
+	 * countTriple("abcXXXabc") → 1
+	 * countTriple("xxxabyyyycd") → 3
+	 * countTriple("a") → 0
+	 */
+	public int countTriple(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
