992df0e8f58082d1d563534ebc86129e76fd0a3e
tun67213 <tun67213@temple.edu>
2024-12-04 19:24:38 -0500

    Added function mirrorEnds()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -113,6 +113,20 @@ public class String_3
 	}
 
 	/**
+	 * Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string.
+	 * In other words, zero or more characters at the very begining of the given string, and at the very end of the
+	 * string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab".
+	 *
+	 * mirrorEnds("abXYZba") → "ab"
+	 * mirrorEnds("abca") → "a"
+	 * mirrorEnds("aba") → "aba"
+	 */
+	public String mirrorEnds(String string)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
