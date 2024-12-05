9f9ad8bbd2d75b3b74e89de973fcca90a9466919
tun67213 <tun67213@temple.edu>
2024-12-04 19:35:24 -0500

    Added function notReplace()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -154,6 +154,20 @@ public class String_3
 	}
 
 	/**
+	 * Given a string, return a string where every appearance of the lowercase word "is" has been replaced with
+	 * "is not". The word "is" should not be immediately preceeded or followed by a letter -- so for example the
+	 * "is" in "this" does not count. (Note: Character.isLetter(char) tests if a char is a letter.)
+	 *
+	 * notReplace("is test") → "is not test"
+	 * notReplace("is-is") → "is not-is not"
+	 * notReplace("This is right") → "This is not right"
+	 */
+	public String notReplace(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
