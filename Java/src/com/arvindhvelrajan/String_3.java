4776f3ec766700f4e4699cad43c11c87f8b04c17
tun67213 <tun67213@temple.edu>
2024-12-04 19:32:37 -0500

    Added function sumNumbers()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -140,6 +140,20 @@ public class String_3
 	}
 
 	/**
+	 * Given a string, return the sum of the numbers appearing in the string, ignoring all other characters.
+	 * A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is
+	 * one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+	 *
+	 * sumNumbers("abc123xyz") → 123
+	 * sumNumbers("aa11b33") → 44
+	 * sumNumbers("7 11") → 18
+	 */
+	public int sumNumbers(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
