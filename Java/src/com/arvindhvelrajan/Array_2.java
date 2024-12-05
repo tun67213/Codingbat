9d786538534daf9e3b081d5996df3e300773a01c
tun67213 <tun67213@temple.edu>
2024-12-05 08:29:16 -0500

    Added function lucky13()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -96,6 +96,18 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if the array contains no 1's and no 3's.
+	 *
+	 * lucky13([0, 2, 4]) → true
+	 * lucky13([1, 2, 3]) → false
+	 * lucky13([1, 2, 4]) → false
+	 */
+	public boolean lucky13(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
