ebef11efcf81ca1efc13c4b6f495acf044a14ce6
tun67213 <tun67213@temple.edu>
2024-12-05 10:01:15 -0500

    Added function isEverywhere()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -207,6 +207,19 @@ public class Array_2
 	}
 
 	/**
+	 * We'll say that a value is "everywhere" in an array if for every pair of adjacent elements in the array,
+	 * at least one of the pair is that value. Return true if the given value is everywhere in the array.
+	 *
+	 * isEverywhere([1, 2, 1, 3], 1) → true
+	 * isEverywhere([1, 2, 1, 3], 2) → false
+	 * isEverywhere([1, 2, 1, 3, 4], 1) → false
+	 */
+	public boolean isEverywhere(int[] nums, int val)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
