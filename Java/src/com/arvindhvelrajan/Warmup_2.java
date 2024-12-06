110a4b4474dc8a16d86c2424f6b7581ec583b9a6
tun67213 <tun67213@temple.edu>
2024-12-06 07:42:31 -0500

    Added function stringYak()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -182,6 +182,19 @@ public class Warmup_2
 	}
 
 	/**
+	 * Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed,
+	 * but the "a" can be any char. The "yak" strings will not overlap.
+	 *
+	 * stringYak("yakpak") → "pak"
+	 * stringYak("pakyak") → "pak"
+	 * stringYak("yak123ya") → "123ya"
+	 */
+	public String stringYak(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
