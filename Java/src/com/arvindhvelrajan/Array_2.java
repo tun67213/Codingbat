7bdaaf68d44f264171ab6871e63a7f73390a3991
tun67213 <tun67213@temple.edu>
2024-12-06 07:59:42 -0500

    Added function matchUp()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -232,6 +232,20 @@ public class Array_2
 	}
 
 	/**
+	 * Given arrays nums1 and nums2 of the same length, for every element in nums1,
+	 * consider the corresponding element in nums2 (at the same index).
+	 * Return the count of the number of times that the two elements differ by 2 or less, but are not equal.
+	 *
+	 * matchUp([1, 2, 3], [2, 3, 10]) → 2
+	 * matchUp([1, 2, 3], [2, 3, 5]) → 3
+	 * matchUp([1, 2, 3], [2, 3, 3]) → 2
+	 */
+	public int matchUp(int[] nums1, int[] nums2)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
