d30fb6f2d4048ebf8803e9e738ad5b7d260b68d9
tun67213 <tun67213@temple.edu>
2024-12-06 07:49:31 -0500

    Added function noTriples()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -208,6 +208,19 @@ public class Warmup_2
 	}
 
 	/**
+	 * Given an array of ints, we'll say that a triple is a value appearing 3 times in a row in the array.
+	 * Return true if the array does not contain any triples.
+	 *
+	 * noTriples([1, 1, 2, 2, 1]) → true
+	 * noTriples([1, 1, 2, 2, 2, 1]) → false
+	 * noTriples([1, 1, 1, 2, 2, 2, 1]) → false
+	 */
+	public boolean noTriples(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
