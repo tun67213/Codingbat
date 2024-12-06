d18cb76f95eaee3cb8c58d806acc37aa519d1e7b
tun67213 <tun67213@temple.edu>
2024-12-06 13:22:51 -0500

    Added function has12()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -259,6 +259,18 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in the array.
+	 *
+	 * has12([1, 3, 2]) → true
+	 * has12([3, 1, 2]) → true
+	 * has12([3, 1, 4, 5, 2]) → true
+	 */
+	public boolean has12(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
