13ce4f704626c8e0068c7cfaabc43a7d3919d172
tun67213 <tun67213@temple.edu>
2024-12-06 13:22:24 -0500

    Separated JavaDoc for has77() into two lines

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -246,7 +246,8 @@ public class Array_2
 	}
 
 	/**
-	 * Given an array of ints, return true if the array contains two 7's next to each other, or there are two 7's separated by one element, such as with {7, 1, 7}.
+	 * Given an array of ints, return true if the array contains two 7's next to each other, or there are two 7's
+	 * separated by one element, such as with {7, 1, 7}.
 	 *
 	 * has77([1, 7, 7]) → true
 	 * has77([1, 7, 1, 7]) → true
