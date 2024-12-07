e3d11d1af1c650d16ec63a80b7ff81b2a2a3fdcc
tun67213 <tun67213@temple.edu>
2024-12-06 21:11:05 -0500

    Added function haveThree()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -283,6 +283,19 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to
+	 * each other.
+	 *
+	 * haveThree([3, 1, 3, 1, 3]) → true
+	 * haveThree([3, 1, 3, 3]) → false
+	 * haveThree([3, 4, 3, 3, 4]) → false
+	 */
+	public boolean haveThree(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
