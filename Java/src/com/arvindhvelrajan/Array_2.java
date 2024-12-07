97495b97f27193b8e6bb7da5782d325dba7bc9d0
tun67213 <tun67213@temple.edu>
2024-12-06 22:04:16 -0500

    Added function shiftLeft()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -349,6 +349,19 @@ public class Array_2
 	}
 
 	/**
+	 * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}.
+	 * You may modify and return the given array, or return a new array.
+	 *
+	 * shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
+	 * shiftLeft([1, 2]) → [2, 1]
+	 * shiftLeft([1]) → [1]
+	 */
+	public int[] shiftLeft(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
