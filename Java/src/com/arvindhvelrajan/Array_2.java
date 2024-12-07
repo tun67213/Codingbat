ece461d400a673e7d7b148c3c5a16cfc183b7c7e
tun67213 <tun67213@temple.edu>
2024-12-06 22:08:50 -0500

    Added function tenRun()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -362,6 +362,19 @@ public class Array_2
 	}
 
 	/**
+	 * For each multiple of 10 in the given array, change all the values following it to be that multiple of 10,
+	 * until encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+	 *
+	 * tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+	 * tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
+	 * tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
+	 */
+	public int[] tenRun(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
