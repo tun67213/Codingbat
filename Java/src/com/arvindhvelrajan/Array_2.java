2554584a45ca443c74af4ae7f6d03b17754b3409
tun67213 <tun67213@temple.edu>
2024-12-06 21:43:19 -0500

    Added function tripleUp()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -322,6 +322,18 @@ public class Array_2
 	}
 
 	/**
+	 * Return true if the array contains, somewhere, three increasing adjacent numbers like .... 4, 5, 6, ... or 23, 24, 25.
+	 *
+	 * tripleUp([1, 4, 5, 6, 2]) → true
+	 * tripleUp([1, 2, 3]) → true
+	 * tripleUp([1, 2, 4]) → false
+	 */
+	public boolean tripleUp(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
