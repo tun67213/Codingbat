646b8aa154080e6788d08bb167c016f70a6247f7
tun67213 <tun67213@temple.edu>
2024-12-07 13:18:31 -0500

    Added function seriesUp()

--- Java/src/com/arvindhvelrajan/Array_3.java
+++ Java/src/com/arvindhvelrajan/Array_3.java
@@ -99,6 +99,20 @@ public class Array_3
 	}
 
 	/**
+	 * Given n>=0, create an array with the pattern {1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n}
+	 * (spaces added to show the grouping).
+	 * Note that the length of the array will be 1 + 2 + 3 ... + n, which is known to sum to exactly n*(n + 1)/2.
+	 *
+	 * seriesUp(3) → [1, 1, 2, 1, 2, 3]
+	 * seriesUp(4) → [1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
+	 * seriesUp(2) → [1, 1, 2]
+	 */
+	public int[] seriesUp(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
