e98f9950dbcf141c1a145e9e541d3fd553b9faf4
tun67213 <tun67213@temple.edu>
2024-12-07 13:58:25 -0500

    Added function mapShare()

--- Java/src/com/arvindhvelrajan/Map_1.java
+++ Java/src/com/arvindhvelrajan/Map_1.java
@@ -31,6 +31,19 @@ public class Map_1
 	}
 
 	/**
+	 * Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value.
+	 * In all cases remove the key "c", leaving the rest of the map unchanged.
+	 *
+	 * mapShare({"a": "aaa", "b": "bbb", "c": "ccc"}) → {"a": "aaa", "b": "aaa"}
+	 * mapShare({"b": "xyz", "c": "ccc"}) → {"b": "xyz"}
+	 * mapShare({"a": "aaa", "c": "meh", "d": "hi"}) → {"a": "aaa", "b": "aaa", "d": "hi"}
+	 */
+	public Map<String, String> mapShare(Map<String, String> map)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
