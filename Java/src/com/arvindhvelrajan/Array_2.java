e7e45db25146ea4c9029f51cec4f4abe0cdd162e
tun67213 <tun67213@temple.edu>
2024-12-06 22:34:45 -0500

    Added function notAlone()Moved post4() to after pre4()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -375,20 +375,6 @@ public class Array_2
 	}
 
 	/**
-	 * Given a non-empty array of ints, return a new array containing the elements from the original array that come
-	 * after the last 4 in the original array. The original array will contain at least one 4.
-	 * Note that it is valid in java to create an array of length 0.
-	 *
-	 * post4([2, 4, 1, 2]) → [1, 2]
-	 * post4([4, 1, 4, 2]) → [2]
-	 * post4([4, 4, 1, 2, 3]) → [1, 2, 3]
-	 */
-	public int[] post4(int[] nums)
-	{
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	/**
 	 * Given a non-empty array of ints, return a new array containing the elements from the original array that
 	 * come before the first 4 in the original array. The original array will contain at least one 4.
 	 * Note that it is valid in java to create an array of length 0.
@@ -398,6 +384,20 @@ public class Array_2
 	 * pre4([1, 4, 4]) → [1]
 	 */
 	public int[] pre4(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
+	 * Given a non-empty array of ints, return a new array containing the elements from the original array that come
+	 * after the last 4 in the original array. The original array will contain at least one 4.
+	 * Note that it is valid in java to create an array of length 0.
+	 *
+	 * post4([2, 4, 1, 2]) → [1, 2]
+	 * post4([4, 1, 4, 2]) → [2]
+	 * post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+	 */
+	public int[] post4(int[] nums)
 	{
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
