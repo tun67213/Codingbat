e20d8010d753c7120fa416be0a439301feba5833
tun67213 <tun67213@temple.edu>
2024-12-06 21:24:10 -0500

    Added function twoTwo()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -296,6 +296,18 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if every 2 that appears in the array is next to another 2.
+	 *
+	 * twoTwo([4, 2, 2, 3]) → true
+	 * twoTwo([2, 2, 4]) → true
+	 * twoTwo([2, 2, 4, 2]) → false
+	 */
+	public boolean twoTwo(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
