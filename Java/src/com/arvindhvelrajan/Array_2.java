0a043125988c999bc1a614970ea0c8dc4a9584b3
tun67213 <tun67213@temple.edu>
2024-12-06 19:46:21 -0500

    Added function modThree(int[] nums)

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -271,6 +271,18 @@ public class Array_2
 	}
 
 	/**
+	 * Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
+	 *
+	 * modThree([2, 1, 3, 5]) → true
+	 * modThree([2, 1, 2, 5]) → false
+	 * modThree([2, 4, 2, 5]) → true
+	 */
+	public boolean modThree(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
