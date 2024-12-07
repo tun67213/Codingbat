6a7e997ffd12dd5c7736e17e82e063b0e314ca5e
tun67213 <tun67213@temple.edu>
2024-12-07 13:10:23 -0500

    Added function linearIn()

--- Java/src/com/arvindhvelrajan/Array_3.java
+++ Java/src/com/arvindhvelrajan/Array_3.java
@@ -72,6 +72,20 @@ public class Array_3
 	}
 
 	/**
+	 * Given two arrays of ints sorted in increasing order, outer and inner, return true if all of the numbers in inner
+	 * appear in outer. The best solution makes only a single "linear" pass of both arrays, taking advantage of the fact
+	 * that both arrays are already in sorted order.
+	 *
+	 * linearIn([1, 2, 4, 6], [2, 4]) → true
+	 * linearIn([1, 2, 4, 6], [2, 3, 4]) → false
+	 * linearIn([1, 2, 4, 4, 6], [2, 4]) → true
+	 */
+	public boolean linearIn(int[] outer, int[] inner)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
