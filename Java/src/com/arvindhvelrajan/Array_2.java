ad5524da46b2835c39c5b875a6ac688ce58a01fe
tun67213 <tun67213@temple.edu>
2024-12-06 22:00:51 -0500

    Added function fizzArray3()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -334,6 +334,21 @@ public class Array_2
 	}
 
 	/**
+	 * Given start and end numbers, return a new array containing the sequence of integers from start up to but not
+	 * including end, so start=5 and end=10 yields {5, 6, 7, 8, 9}.
+	 * The end number will be greater or equal to the start number.
+	 * Note that a length-0 array is valid. (See also: FizzBuzz Code)
+	 *
+	 * fizzArray3(5, 10) → [5, 6, 7, 8, 9]
+	 * fizzArray3(11, 18) → [11, 12, 13, 14, 15, 16, 17]
+	 * fizzArray3(1, 3) → [1, 2]
+	 */
+	public int[] fizzArray3(int start, int end)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
