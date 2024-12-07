2cc44945f056213b1c4011e2d2471c0b015f5097
tun67213 <tun67213@temple.edu>
2024-12-07 13:28:20 -0500

    Added function countClumps()

--- Java/src/com/arvindhvelrajan/Array_3.java
+++ Java/src/com/arvindhvelrajan/Array_3.java
@@ -128,6 +128,19 @@ public class Array_3
 	}
 
 	/**
+	 * Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value.
+	 * Return the number of clumps in the given array.
+	 *
+	 * countClumps([1, 2, 2, 3, 4, 4]) → 2
+	 * countClumps([1, 1, 2, 1, 1]) → 2
+	 * countClumps([1, 1, 1, 1, 1]) → 1
+	 */
+	public int countClumps(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
