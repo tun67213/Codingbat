078fcf6e3538aaaeb1200467bead154cf6ec2be4
tun67213 <tun67213@temple.edu>
2024-12-07 10:18:40 -0500

    Added function withoutTen()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -431,6 +431,20 @@ public class Array_2
 	}
 
 	/**
+	 * Return a version of the given array where all the 10's have been removed. The remaining elements should shift
+	 * left towards the start of the array as needed, and the empty spaces at the end of the array should be 0.
+	 * So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify and return the given array or make a new array.
+	 *
+	 * withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
+	 * withoutTen([10, 2, 10]) → [2, 0, 0]
+	 * withoutTen([1, 99, 10]) → [1, 99, 0]
+	 */
+	public int[] withoutTen(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
