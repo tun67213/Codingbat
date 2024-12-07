9fd632996f3014a4e9b84b389f62ac4ce8d43bbd
tun67213 <tun67213@temple.edu>
2024-12-06 22:35:36 -0500

    Added function notAlone()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -403,6 +403,20 @@ public class Array_2
 	}
 
 	/**
+	 * We'll say that an element in an array is "alone" if there are values before and after it, and those values are
+	 * different from it. Return a version of the given array where every instance of the given value which is alone is
+	 * replaced by whichever value to its left or right is larger.
+	 *
+	 * notAlone([1, 2, 3], 2) → [1, 3, 3]
+	 * notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
+	 * notAlone([3, 4], 3) → [3, 4]
+	 */
+	public int[] notAlone(int[] nums, int val)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
