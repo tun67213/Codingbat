35778da734206314f67697f528b80a200033e38f
tun67213 <tun67213@temple.edu>
2024-12-06 22:47:08 -0500

    Added function zeroFront()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -417,6 +417,20 @@ public class Array_2
 	}
 
 	/**
+	 * Return an array that contains the exact same numbers as the given array, but rearranged so that all the zeros are
+	 * grouped at the start of the array. The order of the non-zero numbers does not matter.
+	 * So {1, 0, 0, 1} becomes {0 ,0, 1, 1}. You may modify and return the given array or make a new array.
+	 *
+	 * zeroFront([1, 0, 0, 1]) → [0, 0, 1, 1]
+	 * zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
+	 * zeroFront([1, 0]) → [0, 1]
+	 */
+	public int[] zeroFront(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
