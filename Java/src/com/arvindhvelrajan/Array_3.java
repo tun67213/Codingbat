0e6973ed4570bb97119a5812fb1c192094817885
tun67213 <tun67213@temple.edu>
2024-12-07 13:21:15 -0500

    Added function maxMirror()

--- Java/src/com/arvindhvelrajan/Array_3.java
+++ Java/src/com/arvindhvelrajan/Array_3.java
@@ -113,6 +113,21 @@ public class Array_3
 	}
 
 	/**
+	 * We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array,
+	 * the same group appears in reverse order.
+	 * For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 3 (the {1, 2, 3} part).
+	 * Return the size of the largest mirror section found in the given array.
+	 *
+	 * maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
+	 * maxMirror([1, 2, 1, 4]) → 3
+	 * maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2
+	 */
+	public int maxMirror(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
