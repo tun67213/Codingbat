90e078478f356238269038556d52589a9325934a
tun67213 <tun67213@temple.edu>
2024-12-07 17:24:41 -0500

    Added function mapAB4()

--- Java/src/com/arvindhvelrajan/Map_1.java
+++ Java/src/com/arvindhvelrajan/Map_1.java
@@ -123,6 +123,20 @@ public class Map_1
 	}
 
 	/**
+	 * Modify and return the given map as follows: if the keys "a" and "b" have values that have different lengths,
+	 * then set "c" to have the longer value. If the values exist and have the same length, change them both to the
+	 * empty string in the map.
+	 *
+	 * mapAB4({"a": "aaa", "b": "bb", "c": "cake"}) → {"a": "aaa", "b": "bb", "c": "aaa"}
+	 * mapAB4({"a": "aa", "b": "bbb", "c": "cake"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+	 * mapAB4({"a": "aa", "b": "bbb"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+	 */
+	public Map<String, String> mapAB4(Map<String, String> map)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
