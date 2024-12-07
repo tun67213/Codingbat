526f056c3077d71af101f937bb210ed11af5b183
tun67213 <tun67213@temple.edu>
2024-12-07 16:38:41 -0500

    Added function mapAB()

--- Java/src/com/arvindhvelrajan/Map_1.java
+++ Java/src/com/arvindhvelrajan/Map_1.java
@@ -44,6 +44,19 @@ public class Map_1
 	}
 
 	/**
+	 * Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys.
+	 * If both keys are present, append their 2 string values together and store the result under the key "ab".
+	 *
+	 * mapAB({"a": "Hi", "b": "There"}) → {"a": "Hi", "ab": "HiThere", "b": "There"}
+	 * mapAB({"a": "Hi"}) → {"a": "Hi"}
+	 * mapAB({"b": "There"}) → {"b": "There"}
+	 */
+	public Map<String, String> mapAB(Map<String, String> map)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
