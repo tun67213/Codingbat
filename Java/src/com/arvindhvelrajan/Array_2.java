4c8a518673ca1df4a8e7175a67f07a1da90e16cf
tun67213 <tun67213@temple.edu>
2024-12-06 22:29:51 -0500

    Added function post4()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -375,6 +375,20 @@ public class Array_2
 	}
 
 	/**
+	 * Given a non-empty array of ints, return a new array containing the elements from the original array that come
+	 * after the last 4 in the original array. The original array will contain at least one 4.
+	 * Note that it is valid in java to create an array of length 0.
+	 *
+	 * post4([2, 4, 1, 2]) → [1, 2]
+	 * post4([4, 1, 4, 2]) → [2]
+	 * post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+	 */
+	public int[] post4(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * Given a non-empty array of ints, return a new array containing the elements from the original array that
 	 * come before the first 4 in the original array. The original array will contain at least one 4.
 	 * Note that it is valid in java to create an array of length 0.
