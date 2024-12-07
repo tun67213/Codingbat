230f3f02e632c67a400ba5d97c492314d3e90c00
tun67213 <tun67213@temple.edu>
2024-12-07 13:37:13 -0500

    Added function mapBully()

--- Java/src/com/arvindhvelrajan/Map_1.java
+++ Java/src/com/arvindhvelrajan/Map_1.java
@@ -14,6 +14,20 @@ public class Map_1
 	}
 
 	/**
+	 * Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value,
+	 * and set the key "a" to have the value "".
+	 * Basically "b" is a bully, taking the value and replacing it with the empty string.
+	 *
+	 * mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
+	 * mapBully({"a": "candy"}) → {"a": "", "b": "candy"}
+	 * mapBully({"a": "candy", "b": "carrot", "c": "meh"}) → {"a": "", "b": "candy", "c": "meh"}
+	 */
+	public Map<String, String> mapBully(Map<String, String> map)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
