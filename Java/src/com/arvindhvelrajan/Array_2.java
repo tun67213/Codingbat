42f1de4101e808048f6443da8d1c420143b204aa
tun67213 <tun67213@temple.edu>
2024-12-07 10:23:37 -0500

    Added function zeroMax()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -445,6 +445,19 @@ public class Array_2
 	}
 
 	/**
+	 * Return a version of the given array where each zero value in the array is replaced by the largest odd value to
+	 * the right of the zero in the array. If there is no odd value to the right of the zero, leave the zero as a zero.
+	 *
+	 * zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
+	 * zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
+	 * zeroMax([0, 1, 0]) → [1, 1, 0]
+	 */
+	public int[] zeroMax(int[] nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
