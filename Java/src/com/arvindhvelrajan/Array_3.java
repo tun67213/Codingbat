95980aa2d34eb8f6f1b938c1b6c78c69a89141ce
tun67213 <tun67213@temple.edu>
2024-12-07 13:16:01 -0500

    Added function squareUp()

--- Java/src/com/arvindhvelrajan/Array_3.java
+++ Java/src/com/arvindhvelrajan/Array_3.java
@@ -86,6 +86,19 @@ public class Array_3
 	}
 
 	/**
+	 * Given n>=0, create an array length n*n with the following pattern,
+	 * shown here for n=3 : {0, 0, 1,    0, 2, 1,    3, 2, 1} (spaces added to show the 3 groups).
+	 *
+	 * squareUp(3) → [0, 0, 1, 0, 2, 1, 3, 2, 1]
+	 * squareUp(2) → [0, 1, 2, 1]
+	 * squareUp(4) → [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
+	 */
+	public int[] squareUp(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
