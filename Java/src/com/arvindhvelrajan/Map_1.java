9e0a2f3faa62fbbdf4f0536df4492fba15a37ece
tun67213 <tun67213@temple.edu>
2024-12-07 16:58:10 -0500

    Added function topping3()

--- Java/src/com/arvindhvelrajan/Map_1.java
+++ Java/src/com/arvindhvelrajan/Map_1.java
@@ -84,6 +84,20 @@ public class Map_1
 	}
 
 	/**
+	 * Given a map of food keys and topping values, modify and return the map as follows: if the key "potato" has a
+	 * value, set that as the value for the key "fries". If the key "salad" has a value, set that as the value for the
+	 * key "spinach".
+	 *
+	 * topping3({"potato": "ketchup"}) → {"potato": "ketchup", "fries": "ketchup"}
+	 * topping3({"potato": "butter"}) → {"potato": "butter", "fries": "butter"}
+	 * topping3({"salad": "oil", "potato": "ketchup"}) → {"spinach": "oil", "salad": "oil", "potato": "ketchup", "fries": "ketchup"}
+	 */
+	public Map<String, String> topping3(Map<String, String> map)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
