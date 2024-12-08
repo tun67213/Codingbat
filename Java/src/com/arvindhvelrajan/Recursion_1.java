3802b04e915f9f6493b17cc33b68ce3735d9473d
tun67213 <tun67213@temple.edu>
2024-12-08 18:27:53 -0500

    Added function count11()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -303,6 +303,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a string, compute recursively (no loops) the number of "11" substrings in the string.
+	 * The "11" substrings should not overlap.
+	 *
+	 * count11("11abc11") → 2
+	 * count11("abc11x11x11") → 3
+	 * count11("111") → 1
+	 */
+	public int count11(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
