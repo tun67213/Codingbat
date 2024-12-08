18250810b760c8d07e3ef7203834de862228042b
tun67213 <tun67213@temple.edu>
2024-12-08 16:09:01 -0500

    Added function bunnyEars2()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -55,6 +55,20 @@ public class Recursion_1
 	}
 
 	/**
+	 * We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears.
+	 * The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot.
+	 * Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+	 *
+	 * bunnyEars2(0) → 0
+	 * bunnyEars2(1) → 2
+	 * bunnyEars2(2) → 5
+	 */
+	public int bunnyEars2(int bunnies)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
