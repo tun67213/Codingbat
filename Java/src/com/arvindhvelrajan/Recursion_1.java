c341ad612976e60026c60bc80b14d5777ca0873a
tun67213 <tun67213@temple.edu>
2024-12-08 16:00:23 -0500

    Added function factorial()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -12,6 +12,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1.
+	 * Compute the result recursively (without loops).
+	 *
+	 * factorial(1) → 1
+	 * factorial(2) → 2
+	 * factorial(3) → 6
+	 */
+	public int factorial(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
