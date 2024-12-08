b00efda989545f7dab028c07fea10d153d0c33c4
tun67213 <tun67213@temple.edu>
2024-12-08 11:53:27 -0500

    Added function wordAppend()

--- Java/src/com/arvindhvelrajan/Map_2.java
+++ Java/src/com/arvindhvelrajan/Map_2.java
@@ -83,6 +83,19 @@ public class Map_2
 	}
 
 	/**
+	 * Loop over the given array of strings to build a result string like this: when a string appears the 2nd, 4th, 6th,
+	 * etc. time in the array, append the string to the result. Return the empty string if no string appears a 2nd time.
+	 *
+	 * wordAppend(["a", "b", "a"]) → "a"
+	 * wordAppend(["a", "b", "a", "c", "a", "d", "a"]) → "aa"
+	 * wordAppend(["a", "", "a"]) → "a"
+	 */
+	public String wordAppend(String[] strings)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * The following function enables a readable output for the reader
 	 */
 	public String toString(Map<String, Integer> map)
