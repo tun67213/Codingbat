5f79779d45434b6d17c271ab6546b6cc786e20fd
tun67213 <tun67213@temple.edu>
2024-12-08 17:55:57 -0500

    Updated array11() function declaration

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -220,7 +220,7 @@ public class Recursion_1
 	 * array11([11, 11], 0) → 2
 	 * array11([1, 2, 3, 4], 0) → 0
 	 */
-	public int array11(int[] nums)
+	public int array11(int[] nums, int index)
 	{
 		throw new UnsupportedOperationException("Not supported yet.");
 	}
