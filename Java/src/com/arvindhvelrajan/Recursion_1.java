660f6251df7301b4e0b1a9bd031bbd7a7b453a22
tun67213 <tun67213@temple.edu>
2024-12-08 16:21:12 -0500

    Added function count8()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -110,6 +110,21 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit,
+	 * except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4.
+	 * Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost
+	 * digit (126 / 10 is 12).
+	 *
+	 * count8(8) → 1
+	 * count8(818) → 2
+	 * count8(8818) → 4
+	 */
+	public int count8(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
