2534d4a04bfab296db2b7620e651b2a78c7c0417
tun67213 <tun67213@temple.edu>
2024-12-08 16:11:37 -0500

    Added function triangle()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -69,6 +69,20 @@ public class Recursion_1
 	}
 
 	/**
+	 * We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has
+	 * 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a
+	 * triangle with the given number of rows.
+	 *
+	 * triangle(0) → 0
+	 * triangle(1) → 1
+	 * triangle(2) → 3
+	 */
+	public int triangle(int rows)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
