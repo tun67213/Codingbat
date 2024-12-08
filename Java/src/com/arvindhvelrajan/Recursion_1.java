ece6820b4f3dfa6579dbc68cec84a438f4efe582
tun67213 <tun67213@temple.edu>
2024-12-08 16:25:31 -0500

    Added function powerN()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -125,6 +125,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given base and n that are both 1 or more, compute recursively (no loops) the value of base to the n power,
+	 * so powerN(3, 2) is 9 (3 squared).
+	 *
+	 * powerN(3, 1) → 3
+	 * powerN(3, 2) → 9
+	 * powerN(3, 3) → 27
+	 */
+	public int powerN(int base, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
