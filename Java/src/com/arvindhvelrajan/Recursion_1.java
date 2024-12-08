e92a353cd02f05cd75e484e91599ac46439fdb8b
tun67213 <tun67213@temple.edu>
2024-12-08 16:16:56 -0500

    Added function count7()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -96,6 +96,20 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2.
+	 * (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+	 * while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+	 *
+	 * count7(717) → 2
+	 * count7(7) → 1
+	 * count7(123) → 0
+	 */
+	public int count7(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
