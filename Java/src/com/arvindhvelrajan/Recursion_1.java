4cf05df1440b1c164859378b642d71586c518d6d
tun67213 <tun67213@temple.edu>
2024-12-08 16:03:55 -0500

    Added function bunnyEars()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -27,6 +27,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * We have a number of bunnies and each bunny has two big floppy ears.
+	 * We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+	 *
+	 * bunnyEars(0) → 0
+	 * bunnyEars(1) → 2
+	 * bunnyEars(2) → 4
+	 */
+	public int bunnyEars(int bunnies)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
