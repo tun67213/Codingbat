0cbeda59df5e3248201696b0304a57b688b68d53
tun67213 <tun67213@temple.edu>
2024-12-08 16:06:32 -0500

    Added function fibonacci()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -40,6 +40,21 @@ public class Recursion_1
 	}
 
 	/**
+	 * The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition.
+	 * The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of
+	 * the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on.
+	 * Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+	 *
+	 * fibonacci(0) → 0
+	 * fibonacci(1) → 1
+	 * fibonacci(2) → 1
+	 */
+	public int fibonacci(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
