8a1d0beed265e523f1727cd64e65bfd95e22c698
tun67213 <tun67213@temple.edu>
2024-12-08 17:51:17 -0500

    Added function array6()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -198,6 +198,20 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given an array of ints, compute recursively if the array contains a 6. We'll use the convention of considering
+	 * only the part of the array that begins at the given index.
+	 * In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+	 *
+	 * array6([1, 6, 4], 0) → true
+	 * array6([1, 4], 0) → false
+	 * array6([6], 0) → true
+	 */
+	public boolean array6(int[] nums, int index)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
