e5b1e9aaee4581513488fb788b40ec23be151cf5
tun67213 <tun67213@temple.edu>
2024-12-08 18:16:38 -0500

    Added function endX()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -265,6 +265,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of
+	 * the string.
+	 *
+	 * endX("xxre") → "rexx"
+	 * endX("xxhixx") → "hixxxx"
+	 * endX("xhixhix") → "hihixxx"
+	 */
+	public String endX(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
