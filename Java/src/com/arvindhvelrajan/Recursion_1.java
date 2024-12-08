4143117914cd8e68c7f81fafc641bfab4ad13f84
tun67213 <tun67213@temple.edu>
2024-12-08 18:59:22 -0500

    Split JavaDoc for countHi2() into two lines

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -329,7 +329,8 @@ public class Recursion_1
 	}
 
 	/**
-	 * Given a string, compute recursively the number of times lowercase "hi" appears in the string, however do not count "hi" that have an 'x' immedately before them.
+	 * Given a string, compute recursively the number of times lowercase "hi" appears in the string, however do not
+	 * count "hi" that have an 'x' immediately before them.
 	 *
 	 * countHi2("ahixhi") → 1
 	 * countHi2("ahibhi") → 2
