ba112b1e3ebd224dc2f6c3951063df6e1b505beb
tun67213 <tun67213@temple.edu>
2024-12-08 11:52:39 -0500

    Split JavaDoc for firstChar() into 2 lines for readability

--- Java/src/com/arvindhvelrajan/Map_2.java
+++ Java/src/com/arvindhvelrajan/Map_2.java
@@ -70,7 +70,8 @@ public class Map_2
 	}
 
 	/**
-	 * Given an array of non-empty strings, return a Map<String, String> with a key for every different first character seen, with the value of all the strings starting with that character appended together in the order they appear in the array.
+	 * Given an array of non-empty strings, return a Map<String, String> with a key for every different first character
+	 * seen, with the value of all the strings starting with that character appended together in the order they appear in the array.
 	 *
 	 * firstChar(["salt", "tea", "soda", "toast"]) → {"s": "saltsoda", "t": "teatoast"}
 	 * firstChar(["aa", "bb", "cc", "aAA", "cCC", "d"]) → {"a": "aaaAA", "b": "bb", "c": "cccCC", "d": "d"}
