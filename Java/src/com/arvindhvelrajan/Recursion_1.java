52aee77f634434cb81ff3489da15daad6a2e1d02
tun67213 <tun67213@temple.edu>
2024-12-08 16:14:09 -0500

    Added function sumDigits()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -83,6 +83,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields
+	 * the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+	 *
+	 * sumDigits(126) → 9
+	 * sumDigits(49) → 13
+	 * sumDigits(12) → 3
+	 */
+	public int sumDigits(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
