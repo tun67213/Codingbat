812ab8947e22e6d4ac2f47117d921c3e679ae718
tun67213 <tun67213@temple.edu>
2024-12-08 17:42:39 -0500

    Added function changePi()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -174,6 +174,18 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+	 *
+	 * changePi("xpix") → "x3.14x"
+	 * changePi("pipi") → "3.143.14"
+	 * changePi("pip") → "3.14p"
+	 */
+	public String changePi(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
