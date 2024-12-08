f201b9c77735c92a4044d645d1966e446f20280e
tun67213 <tun67213@temple.edu>
2024-12-08 16:05:49 -0500

    Added all test cases for bunnyEars() to main()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -55,5 +55,15 @@ public class Recursion_1
 		System.out.println("factorial(7) → " + factorial(7));
 		System.out.println("factorial(8) → " + factorial(8));
 		System.out.println("factorial(12) → " + factorial(12));
+		printFunction("bunnyEars");
+		System.out.println("bunnyEars(0) → " + bunnyEars(0));
+		System.out.println("bunnyEars(1) → " + bunnyEars(1));
+		System.out.println("bunnyEars(2) → " + bunnyEars(2));
+		System.out.println("bunnyEars(3) → " + bunnyEars(3));
+		System.out.println("bunnyEars(4) → " + bunnyEars(4));
+		System.out.println("bunnyEars(5) → " + bunnyEars(5));
+		System.out.println("bunnyEars(12) → " + bunnyEars(12));
+		System.out.println("bunnyEars(50) → " + bunnyEars(50));
+		System.out.println("bunnyEars(234) → " + bunnyEars(234));
 	}
 }
