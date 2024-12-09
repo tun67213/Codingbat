0682e0cc62ecb4f522a1a03b85834f5064cfb554
tun67213 <tun67213@temple.edu>
2024-12-09 18:45:03 -0500

    Added function lower()

--- Java/src/com/arvindhvelrajan/Functional_1.java
+++ Java/src/com/arvindhvelrajan/Functional_1.java
@@ -101,6 +101,19 @@ public class Functional_1
 	}
 
 	/**
+	 * Given a list of strings, return a list where each string is converted to lower case
+	 * (Note: String toLowerCase() method).
+	 *
+	 * lower(["Hello", "Hi"]) → ["hello", "hi"]
+	 * lower(["AAA", "BBB", "ccc"]) → ["aaa", "bbb", "ccc"]
+	 * lower(["KitteN", "ChocolaTE"]) → ["kitten", "chocolate"]
+	 */
+	public List<String> lower(List<String> strings)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * This is a helper function that adds quotations to strings in a list
 	 */
 	public String toStringStringList(List<String> list)
