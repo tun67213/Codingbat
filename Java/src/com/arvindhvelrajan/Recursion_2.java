50a4fa1c3c304d4eaa955f3e82e4fc72e5e55f0b
tun67213 <tun67213@temple.edu>
2024-12-08 21:57:02 -0500

    Added function groupSum6()

--- Java/src/com/arvindhvelrajan/Recursion_2.java
+++ Java/src/com/arvindhvelrajan/Recursion_2.java
@@ -31,6 +31,20 @@ public class Recursion_2
 	}
 
 	/**
+	 * Given an array of ints, is it possible to choose a group of some of the ints, beginning at the start index, such
+	 * that the group sums to the given target? However, with the additional constraint that all 6's must be chosen.
+	 * (No loops needed.)
+	 *
+	 * groupSum6(0, [5, 6, 2], 8) → true
+	 * groupSum6(0, [5, 6, 2], 9) → false
+	 * groupSum6(0, [5, 6, 2], 7) → false
+	 */
+	public boolean groupSum6(int start, int[] nums, int target)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
