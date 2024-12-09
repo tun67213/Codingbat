b01a1c566e52fe3c3bee3346bb9c89fab49951de
tun67213 <tun67213@temple.edu>
2024-12-09 08:02:36 -0500

    Added function addStar()

--- Java/src/com/arvindhvelrajan/Functional_1.java
+++ Java/src/com/arvindhvelrajan/Functional_1.java
@@ -41,6 +41,18 @@ public class Functional_1
 	}
 
 	/**
+	 * Given a list of strings, return a list where each string has "*" added at its end.
+	 *
+	 * addStar(["a", "bb", "ccc"]) → ["a*", "bb*", "ccc*"]
+	 * addStar(["hello", "there"]) → ["hello*", "there*"]
+	 * addStar(["*"]) → ["**"]
+	 */
+	public List<String> addStar(List<String> strings)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
