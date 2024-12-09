f7821359cc2a6bd57afb27aa126c5479710e1807
tun67213 <tun67213@temple.edu>
2024-12-08 19:54:41 -0500

    Added function strCopies()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -381,6 +381,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a string and a non-empty substring sub, compute recursively if at least n copies of sub appear in the
+	 * string somewhere, possibly with overlapping. N will be non-negative.
+	 *
+	 * strCopies("catcowcat", "cat", 2) → true
+	 * strCopies("catcowcat", "cow", 2) → false
+	 * strCopies("catcowcat", "cow", 1) → true
+	 */
+	public boolean strCopies(String str, String sub, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
