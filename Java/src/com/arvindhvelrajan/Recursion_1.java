1d7abc0fc22befd29c787e41cbd620c1dcc18445
tun67213 <tun67213@temple.edu>
2024-12-08 19:44:14 -0500

    Added function nestParen()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -355,6 +355,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a string, return true if it is a nesting of zero or more pairs of parenthesis, like "(())" or "((()))".
+	 * Suggestion: check the first and last chars, and then recur on what's inside them.
+	 *
+	 * nestParen("(())") → true
+	 * nestParen("((()))") → true
+	 * nestParen("(((x))") → false
+	 */
+	public boolean nestParen(String str)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
