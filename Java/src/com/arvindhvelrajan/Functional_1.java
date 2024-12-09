542c3b19770dbab5003da1ecff6e3a6f2d6073ba
tun67213 <tun67213@temple.edu>
2024-12-09 18:35:43 -0500

    Added function math1()

--- Java/src/com/arvindhvelrajan/Functional_1.java
+++ Java/src/com/arvindhvelrajan/Functional_1.java
@@ -77,6 +77,18 @@ public class Functional_1
 	}
 
 	/**
+	 * Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+	 *
+	 * math1([1, 2, 3]) → [20, 30, 40]
+	 * math1([6, 8, 6, 8, 1]) → [70, 90, 70, 90, 20]
+	 * math1([10]) → [110]
+	 */
+	public List<Integer> math1(List<Integer> nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * This is a helper function that adds quotations to strings in a list
 	 */
 	public String toStringStringList(List<String> list)
