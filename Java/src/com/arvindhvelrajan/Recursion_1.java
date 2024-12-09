7007255d47d74a7a6d504b6b5eb413ffdcd07742
tun67213 <tun67213@temple.edu>
2024-12-08 19:50:26 -0500

    Added function strCount()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -368,6 +368,19 @@ public class Recursion_1
 	}
 
 	/**
+	 * Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the
+	 * string, without the sub strings overlapping.
+	 *
+	 * strCount("catcowcat", "cat") → 2
+	 * strCount("catcowcat", "cow") → 1
+	 * strCount("catcowcat", "dog") → 0
+	 */
+	public int strCount(String str, String sub)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
