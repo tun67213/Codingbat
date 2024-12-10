8507fec092db231dcd590b3904240f74e656a07b
tun67213 <tun67213@temple.edu>
2024-12-09 20:42:36 -0500

    Added function two2()

--- Java/src/com/arvindhvelrajan/Functional_2.java
+++ Java/src/com/arvindhvelrajan/Functional_2.java
@@ -102,6 +102,19 @@ public class Functional_2
 	}
 
 	/**
+	 * Given a list of non-negative integers, return a list of those numbers multiplied by 2, omitting any of the
+	 * resulting numbers that end in 2.
+	 *
+	 * two2([1, 2, 3]) → [4, 6]
+	 * two2([2, 6, 11]) → [4]
+	 * two2([0]) → [0]
+	 */
+	public List<Integer> two2(List<Integer> nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * This is a helper function that adds quotations to strings in a list
 	 */
 	public String toStringStringList(List<String> list)
