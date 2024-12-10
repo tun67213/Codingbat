c6f247648e84092aae7b0c39f4deaa6c29bf1018
tun67213 <tun67213@temple.edu>
2024-12-09 20:48:14 -0500

    Added function square56()

--- Java/src/com/arvindhvelrajan/Functional_2.java
+++ Java/src/com/arvindhvelrajan/Functional_2.java
@@ -115,6 +115,19 @@ public class Functional_2
 	}
 
 	/**
+	 * Given a list of integers, return a list of those numbers squared and the product added to 10, omitting any of
+	 * the resulting numbers that end in 5 or 6.
+	 *
+	 * square56([3, 1, 4]) → [19, 11]
+	 * square56([1]) → [11]
+	 * square56([2]) → [14]
+	 */
+	public List<Integer> square56(List<Integer> nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * This is a helper function that adds quotations to strings in a list
 	 */
 	public String toStringStringList(List<String> list)
