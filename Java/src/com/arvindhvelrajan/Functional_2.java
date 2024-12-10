b6270231a85417ee831bd6b08134d62369aa5364
tun67213 <tun67213@temple.edu>
2024-12-09 20:34:25 -0500

    Added function noYY()

--- Java/src/com/arvindhvelrajan/Functional_2.java
+++ Java/src/com/arvindhvelrajan/Functional_2.java
@@ -89,6 +89,19 @@ public class Functional_2
 	}
 
 	/**
+	 * Given a list of strings, return a list where each string has "y" added at its end, omitting any resulting
+	 * strings that contain "yy" as a substring anywhere.
+	 *
+	 * noYY(["a", "b", "c"]) → ["ay", "by", "cy"]
+	 * noYY(["a", "b", "cy"]) → ["ay", "by"]
+	 * noYY(["xx", "ya", "zz"]) → ["xxy", "yay", "zzy"]
+	 */
+	public List<String> noYY(List<String> strings)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * This is a helper function that adds quotations to strings in a list
 	 */
 	public String toStringStringList(List<String> list)
