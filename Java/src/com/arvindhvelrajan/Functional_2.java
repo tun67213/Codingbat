186b9a367d20525767ef4ee9135e30745d2a944e
tun67213 <tun67213@temple.edu>
2024-12-09 19:23:22 -0500

    Added function noNeg()

--- Java/src/com/arvindhvelrajan/Functional_2.java
+++ Java/src/com/arvindhvelrajan/Functional_2.java
@@ -14,6 +14,18 @@ public class Functional_2
 	}
 
 	/**
+	 * Given a list of integers, return a list of the integers, omitting any that are less than 0.
+	 *
+	 * noNeg([1, -2]) → [1]
+	 * noNeg([-3, -3, 3, 3]) → [3, 3]
+	 * noNeg([-1, -1, -1]) → []
+	 */
+	public List<Integer> noNeg(List<Integer> nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
