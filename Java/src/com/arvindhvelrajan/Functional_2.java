777b9105e1e849866c613a8dd59197d480277710
tun67213 <tun67213@temple.edu>
2024-12-09 19:57:17 -0500

    Added function noTeen()

--- Java/src/com/arvindhvelrajan/Functional_2.java
+++ Java/src/com/arvindhvelrajan/Functional_2.java
@@ -41,6 +41,18 @@ public class Functional_2
 	}
 
 	/**
+	 * Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive.
+	 *
+	 * noTeen([12, 13, 19, 20]) → [12, 20]
+	 * noTeen([1, 14, 1]) → [1, 1]
+	 * noTeen([15]) → []
+	 */
+	public List<Integer> noTeen(List<Integer> nums)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
