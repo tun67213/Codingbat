a2ef608c80638324f4bb7089393bfeb5893fca05
tun67213 <tun67213@temple.edu>
2024-12-10 08:52:31 -0500

    Implemented iterative algorithm for countXX()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -88,7 +88,19 @@ public class Warmup_2
 	 */
 	public int countXX(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() < 2)
+		{
+			return 0;
+		}
+		int count = 0;
+		for(int i = 0; i < str.length() - 1; i++)
+		{
+			if(str.substring(i, i + 2).equals("xx"))
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
