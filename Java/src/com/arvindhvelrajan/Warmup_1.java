2e61b933f76399db1106a355d1492ae76da0e280
tun67213 <tun67213@temple.edu>
2024-12-09 22:38:15 -0500

    Implemented backAround()

--- Java/src/com/arvindhvelrajan/Warmup_1.java
+++ Java/src/com/arvindhvelrajan/Warmup_1.java
@@ -219,7 +219,11 @@ public class Warmup_1
 	 */
 	public String backAround(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() <= 1)
+		{
+			return str + str + str;
+		}
+		return str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
 	}
 
 	/**
