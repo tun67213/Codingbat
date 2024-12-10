b68fc369bc1dec8f6fca454dcb3e87106ae2b71e
tun67213 <tun67213@temple.edu>
2024-12-10 17:29:34 -0500

    Added iterative implementation for stringMatch()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -368,7 +368,16 @@ public class Warmup_2
 	 */
 	public int stringMatch(String a, String b)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		String minString = (a.length() < b.length()) ? a : b;
+		int count = 0;
+		for(int i = 0; i < minString.length() - 1; i++)
+		{
+			if(a.substring(i, i + 2).equals(b.substring(i, i + 2)))
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
