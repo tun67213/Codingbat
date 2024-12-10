17190a9cfb756cf688f601da78f70ce7f4352f65
tun67213 <tun67213@temple.edu>
2024-12-10 09:41:06 -0500

    Implemented function arrayCount9()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -269,7 +269,15 @@ public class Warmup_2
 	 */
 	public int arrayCount9(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int count = 0;
+		for(int num : nums)
+		{
+			if(num == 9)
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
