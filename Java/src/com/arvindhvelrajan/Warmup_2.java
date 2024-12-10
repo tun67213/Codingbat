809542c144ae560cd74284d16c3ad052a63a5fc5
tun67213 <tun67213@temple.edu>
2024-12-10 17:23:50 -0500

    Added an iterative implementation for array123()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -342,7 +342,14 @@ public class Warmup_2
 	 */
 	public boolean array123(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		for(int i = 0; i < nums.length - 2; i++)
+		{
+			if(nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
