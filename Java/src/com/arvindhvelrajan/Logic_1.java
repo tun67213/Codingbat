bef3b969263601cb044f6b8ac844b958f8dc1eff
tun67213 <tun67213@temple.edu>
2024-12-11 17:07:26 -0500

    Implemented method teaParty()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -307,7 +307,15 @@ public class Logic_1
 	 */
 	public int teaParty(int tea, int candy)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(tea < 5 || candy < 5)
+		{
+			return 0;
+		}
+		if(tea >= 2 * candy || candy >= 2 * tea)
+		{
+			return 2;
+		}
+		return 1;
 	}
 
 	/**
