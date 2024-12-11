95048535692ad62822685a3140966fc0f04b3308
tun67213 <tun67213@temple.edu>
2024-12-11 15:21:05 -0500

    Implemented method unlucky1()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -467,7 +467,22 @@ public class Array_1
 	 */
 	public boolean unlucky1(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length < 2)
+		{
+			return false;
+		}
+		for(int i = 0; i < 2; i++)
+		{
+			if(nums[i] == 1 && nums[i + 1] == 3)
+			{
+				return true;
+			}
+		}
+		if(nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3)
+		{
+			return true;
+		}
+		return false;
 	}
 
 	/**
