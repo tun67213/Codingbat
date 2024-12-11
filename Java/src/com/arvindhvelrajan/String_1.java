7eff8c33eb7379c42de4169aca298f4a00bf17f4
tun67213 <tun67213@temple.edu>
2024-12-11 09:13:14 -0500

    Implemented method conCat()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -413,7 +413,11 @@ public class String_1
 	 */
 	public String conCat(String a, String b)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(!a.isEmpty() && !b.isEmpty() && a.substring(a.length() - 1).equals(b.substring(0, 1)))
+		{
+			return a + b.substring(1);
+		}
+		return a + b;
 	}
 
 	/**
