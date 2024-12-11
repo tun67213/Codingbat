0d1582f5b2a987d463e1b7b08fd4248853164a69
tun67213 <tun67213@temple.edu>
2024-12-11 09:25:35 -0500

    Implemented method startWord()

--- Java/src/com/arvindhvelrajan/String_1.java
+++ Java/src/com/arvindhvelrajan/String_1.java
@@ -578,7 +578,11 @@ public class String_1
 	 */
 	public String startWord(String str, String word)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() >= word.length() && str.substring(1, word.length()).equals(word.substring(1)))
+		{
+			return str.substring(0, word.length());
+		}
+		return "";
 	}
 
 	/**
