324538b6fdb7373d5fcf46e241652d6759d884cb
tun67213 <tun67213@temple.edu>
2024-12-11 14:39:46 -0500

    Implemented method swapEnds()

--- Java/src/com/arvindhvelrajan/Array_1.java
+++ Java/src/com/arvindhvelrajan/Array_1.java
@@ -391,7 +391,14 @@ public class Array_1
 	 */
 	public int[] swapEnds(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length <= 1)
+		{
+			return nums;
+		}
+		int temp = nums[0];
+		nums[0] = nums[nums.length - 1];
+		nums[nums.length - 1] = temp;
+		return nums;
 	}
 
 	/**
