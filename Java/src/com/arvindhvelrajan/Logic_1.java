b20ea66b9d2cd884ae04b1e361665afecd787ce6
tun67213 <tun67213@temple.edu>
2024-12-11 17:28:11 -0500

    Implemented method blueTicket()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -556,7 +556,16 @@ public class Logic_1
 	 */
 	public int blueTicket(int a, int b, int c)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int ab = a + b, bc = b + c, ac = a + c;
+		if(ab == 10 || bc == 10 || ac == 10)
+		{
+			return 10;
+		}
+		if(ab == bc + 10 || ab == ac + 10)
+		{
+			return 5;
+		}
+		return 0;
 	}
 
 	/**
