0274d27d8060f6258f6df46d064e592505d9128d
tun67213 <tun67213@temple.edu>
2024-12-10 20:44:59 -0500

    Implemented function noTriples()

--- Java/src/com/arvindhvelrajan/Warmup_2.java
+++ Java/src/com/arvindhvelrajan/Warmup_2.java
@@ -529,7 +529,18 @@ public class Warmup_2
 	 */
 	public boolean noTriples(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length < 3)
+		{
+			return true;
+		}
+		for(int i = 0; i < nums.length - 2; i++)
+		{
+			if(nums[i] == nums[i + 1] && nums[i] == nums[i + 2])
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
