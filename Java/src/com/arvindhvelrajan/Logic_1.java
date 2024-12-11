26cbd700f2423f2305a9c1671612d1fde306dbe3
tun67213 <tun67213@temple.edu>
2024-12-11 15:27:46 -0500

    Implemented method squirrelPlay()

--- Java/src/com/arvindhvelrajan/Logic_1.java
+++ Java/src/com/arvindhvelrajan/Logic_1.java
@@ -73,7 +73,11 @@ public class Logic_1
 	 */
 	public boolean squirrelPlay(int temp, boolean isSummer)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(isSummer)
+		{
+			return temp >= 60 && temp <= 100;
+		}
+		return temp >= 60 && temp <= 90;
 	}
 
 	/**
