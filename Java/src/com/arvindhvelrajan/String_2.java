1760a8902cebf53bf7b6f903853e97cb5052267b
tun67213 <tun67213@temple.edu>
2024-12-12 09:19:47 -0500

    Added flag to suppress all warnings

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -6,6 +6,7 @@ import static com.arvindhvelrajan.UI_Functions.*;
  * @author arvindhvelrajan
  * @code This class is to demonstrate the implementation for all functions in String-2 from codingbat.com
  */
+@SuppressWarnings("ALL")
 public class String_2
 {
 	/**
