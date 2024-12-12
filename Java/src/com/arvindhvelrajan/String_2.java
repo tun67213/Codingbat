9af1a00f4cea4c413cc576e5739ae15d7afcb36c
tun67213 <tun67213@temple.edu>
2024-12-12 09:25:45 -0500

    Updated loop for countCode() to run without exceptions

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -111,7 +111,7 @@ public class String_2
 	public int countCode(String str)
 	{
 		int count = 0;
-		for(int i = 0; i < str.length() - 2; i++)
+		for(int i = 0; i < str.length() - 4; i++)
 		{
 			if(str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e')
 			{
