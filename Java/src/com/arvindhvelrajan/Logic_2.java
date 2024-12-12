9e52539e28ee8a033c369c0cb5976a2fd456f6f4
tun67213 <tun67213@temple.edu>
2024-12-12 07:46:29 -0500

    Implemented method luckySum()

--- Java/src/com/arvindhvelrajan/Logic_2.java
+++ Java/src/com/arvindhvelrajan/Logic_2.java
@@ -67,7 +67,19 @@ public class Logic_2
 	 */
 	public int luckySum(int a, int b, int c)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(a == 13)
+		{
+			return 0;
+		}
+		if(b == 13)
+		{
+			return a;
+		}
+		if(c == 13)
+		{
+			return a + b;
+		}
+		return a + b + c;
 	}
 
 	/**
