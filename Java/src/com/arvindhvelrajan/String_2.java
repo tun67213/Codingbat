ce6ecae8ed34459df5d6fa038559589dde475df7
tun67213 <tun67213@temple.edu>
2024-12-12 09:13:44 -0500

    Implemented method countHi()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -51,7 +51,15 @@ public class String_2
 	 */
 	public int countHi(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int count = 0;
+		for(int i = 0; i < str.length() - 1; i++)
+		{
+			if(str.substring(i, i + 2).equals("hi"))
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
