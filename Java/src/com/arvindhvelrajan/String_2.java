0df9aaf7ed28f22e1677d166fad5b7300db735cc
tun67213 <tun67213@temple.edu>
2024-12-12 16:38:48 -0500

    Implemented method getSandwich()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -434,7 +434,29 @@ public class String_2
 	 */
 	public String getSandwich(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int first = -1;
+		int last = -1;
+		for(int i = 0; i < str.length() - 5; i++)
+		{
+			if(str.substring(i, i + 5).equals("bread"))
+			{
+				first = i;
+				break;
+			}
+		}
+		for(int i = str.length() - 5; i >= 0; i--)
+		{
+			if(str.substring(i, i + 5).equals("bread"))
+			{
+				last = i;
+				break;
+			}
+		}
+		if(first != -1 && last != -1 && first != last)
+		{
+			return str.substring(first + 5, last);
+		}
+		return "";
 	}
 
 	/**
