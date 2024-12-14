22b185824460555337ec5b70bc32929d0681ca4b
tun67213 <tun67213@temple.edu>
2024-12-14 15:21:17 -0500

    Implemented method countTriple()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -202,7 +202,15 @@ public class String_3
 	 */
 	public int countTriple(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int count = 0;
+		for(int i = 0; i < str.length() - 2; i++)
+		{
+			if(str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2))
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
