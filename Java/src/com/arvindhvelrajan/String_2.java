f0d884988d2af401bd44a84c30050ce355d5e2d7
tun67213 <tun67213@temple.edu>
2024-12-14 08:54:51 -0500

    Implemented method starOut()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -564,7 +564,48 @@ public class String_2
 	 */
 	public String starOut(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() < 1)
+		{
+			return "";
+		}
+		if(str.length() == 1)
+		{
+			if(str.charAt(0) == '*')
+			{
+				return "";
+			}
+			else
+			{
+				return str;
+			}
+		}
+
+		char[] arr = new char[str.length()];
+		int count = 0;
+
+
+		if(str.charAt(0) != '*' && str.charAt(1) != '*')
+		{
+			arr[count] = str.charAt(0);
+			count++;
+		}
+
+		for(int i = 1; i < str.length() - 1; i++)
+		{
+			if(str.charAt(i-1)!='*' && str.charAt(i) != '*' && str.charAt(i+1) != '*')
+			{
+				arr[count] = str.charAt(i);
+				count++;
+			}
+		}
+
+		if(str.charAt(str.length()-1) != '*' && str.charAt(str.length()-2) != '*')
+		{
+			arr[count] = str.charAt(str.length() - 1);
+			count++;
+		}
+
+		return new String(arr, 0, count);
 	}
 
 	/**
