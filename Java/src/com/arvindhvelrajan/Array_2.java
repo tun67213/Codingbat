eb23198b68d3009c8dfa4b1f5024f4bc24d0d93e
tun67213 <tun67213@temple.edu>
2024-12-14 16:46:49 -0500

    Implemented method either24()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -383,7 +383,19 @@ public class Array_2
 	 */
 	public boolean either24(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		boolean found2 = false, found4 = false;
+		for(int i = 0; i < nums.length - 1; i++)
+		{
+			if(nums[i] == 2 && nums[i + 1] == 2)
+			{
+				found2 = true;
+			}
+			if(nums[i] == 4 && nums[i + 1] == 4)
+			{
+				found4 = true;
+			}
+		}
+		return found2 ^ found4;
 	}
 
 	/**
