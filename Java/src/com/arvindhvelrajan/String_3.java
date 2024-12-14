14692e79ff551e3701eb5ba79b8e74d8a360fbcc
tun67213 <tun67213@temple.edu>
2024-12-14 15:12:00 -0500

    Implemented method equalIsNot()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -125,7 +125,27 @@ public class String_3
 	 */
 	public boolean equalIsNot(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() <= 1)
+		{
+			return true;
+		}
+		int countIs = 0, countNot = 0;
+		for(int i = 0; i < str.length() - 2; i++)
+		{
+			if(str.substring(i, i + 2).equals("is"))
+			{
+				countIs++;
+			}
+			if(str.substring(i, i + 3).equals("not"))
+			{
+				countNot++;
+			}
+		}
+		if(str.endsWith("is"))
+		{
+			countIs++;
+		}
+		return countIs == countNot;
 	}
 
 	/**
