7b5b23a79c9e306b1dc5b0fac4ed56d9cba3639e
tun67213 <tun67213@temple.edu>
2024-12-14 16:45:06 -0500

    Implemented method isEverywhere()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -360,7 +360,18 @@ public class Array_2
 	 */
 	public boolean isEverywhere(int[] nums, int val)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length == 0)
+		{
+			return true;
+		}
+		for(int i = 0; i < nums.length - 1; i++)
+		{
+			if(!(nums[i] == val || nums[i + 1] == val))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
