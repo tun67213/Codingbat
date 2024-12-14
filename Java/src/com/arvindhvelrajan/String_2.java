fb3eac0acc77e94d0546149095e9da46ddb6d2d5
tun67213 <tun67213@temple.edu>
2024-12-14 09:11:49 -0500

    Implemented method wordEnds()

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -660,7 +660,24 @@ public class String_2
 	 */
 	public String wordEnds(String str, String word)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		StringBuilder result = new StringBuilder();
+		int wordLen = word.length();
+
+		for(int i = 0; i <= str.length() - wordLen; i++)
+		{
+			if(str.substring(i, i + wordLen).equals(word))
+			{
+				if(i > 0)
+				{
+					result.append(str.charAt(i - 1));
+				}
+				if(i + wordLen < str.length())
+				{
+					result.append(str.charAt(i + wordLen));
+				}
+			}
+		}
+		return result.toString();
 	}
 
 	/**
