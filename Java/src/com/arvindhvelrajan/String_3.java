3ebae039e52abfe0751aac37735e498b34770636
tun67213 <tun67213@temple.edu>
2024-12-14 15:25:11 -0500

    Implemented an inductive method of sumDigits()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -240,7 +240,15 @@ public class String_3
 	 */
 	public int sumDigits(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int sum = 0;
+		for(int i = 0; i < str.length(); i++)
+		{
+			if(Character.isDigit(str.charAt(i)))
+			{
+				sum += Integer.parseInt(str.substring(i, i + 1));
+			}
+		}
+		return sum;
 	}
 
 	/**
