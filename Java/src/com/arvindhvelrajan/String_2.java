913474b96e081aadf24bd23cd3d85febfa06c5d1
tun67213 <tun67213@temple.edu>
2024-12-14 09:03:14 -0500

    Implemented plusOut() using loops

--- Java/src/com/arvindhvelrajan/String_2.java
+++ Java/src/com/arvindhvelrajan/String_2.java
@@ -618,7 +618,22 @@ public class String_2
 	 */
 	public String plusOut(String str, String word)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		StringBuilder result = new StringBuilder();
+		int i = 0;
+		while(i < str.length())
+		{
+			if(i <= str.length() - word.length() && str.substring(i, i + word.length()).equals(word))
+			{
+				result.append(word);
+				i += word.length();
+			}
+			else
+			{
+				result.append("+");
+				i++;
+			}
+		}
+		return result.toString();
 	}
 
 	/**
