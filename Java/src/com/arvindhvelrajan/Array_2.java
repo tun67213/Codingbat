eb24deb584f28c5fb218c9e3e2a28e967dd4136b
tun67213 <tun67213@temple.edu>
2024-12-14 16:56:13 -0500

    Implemented method modThree()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -490,7 +490,22 @@ public class Array_2
 	 */
 	public boolean modThree(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length < 3)
+		{
+			return false;
+		}
+		for(int i = 0; i < nums.length - 2; i++)
+		{
+			if(nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0)
+			{
+				return true;
+			}
+			else if(nums[i] % 2 == 1 && nums[i + 1] % 2 == 1 && nums[i + 2] % 2 == 1)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
