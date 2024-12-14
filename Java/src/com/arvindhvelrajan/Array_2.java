2ccbc7db854616050178b759c1e33b45b2ada5d5
tun67213 <tun67213@temple.edu>
2024-12-14 17:06:21 -0500

    Implemented method tripleUp()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -620,7 +620,18 @@ public class Array_2
 	 */
 	public boolean tripleUp(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length < 3)
+		{
+			return false;
+		}
+		for(int i = 0; i < nums.length - 2; i++)
+		{
+			if(nums[i] + 1 == nums[i + 1] && nums[i + 1] + 1 == nums[i + 2])
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
