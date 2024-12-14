e193f99f0ab08b9b59d533de2e648032195efd8b
tun67213 <tun67213@temple.edu>
2024-12-14 15:37:53 -0500

    Implemented method maxBlock()

--- Java/src/com/arvindhvelrajan/String_3.java
+++ Java/src/com/arvindhvelrajan/String_3.java
@@ -324,7 +324,28 @@ public class String_3
 	 */
 	public int maxBlock(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.isEmpty())
+		{
+			return 0;
+		}
+		int largest = 0;
+		int current = 1;
+		for(int i = 1; i < str.length(); i++)
+		{
+			if(str.charAt(i) != str.charAt(i-1))
+			{
+				if(current > largest)
+				{
+					largest = current;
+				}
+				current = 1;
+			}
+			else
+			{
+				current++;
+			}
+		}
+		return Math.max(largest, current);
 	}
 
 	/**
