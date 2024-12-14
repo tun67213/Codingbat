f0e897d6eae1a9d6f81c4a04f50e35ca162885d2
tun67213 <tun67213@temple.edu>
2024-12-14 16:15:03 -0500

    Implemented method has22()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -171,7 +171,18 @@ public class Array_2
 	 */
 	public boolean has22(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length < 2)
+		{
+			return false;
+		}
+		for(int i = 0; i < nums.length - 1; i++)
+		{
+			if(nums[i] == 2 && nums[i + 1] == 2)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
