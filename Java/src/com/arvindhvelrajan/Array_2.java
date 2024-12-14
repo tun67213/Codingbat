8747d51df2ce73ff582998989694d1191f8654d0
tun67213 <tun67213@temple.edu>
2024-12-14 16:58:40 -0500

    Implemented method haveThree()

--- Java/src/com/arvindhvelrajan/Array_2.java
+++ Java/src/com/arvindhvelrajan/Array_2.java
@@ -518,7 +518,19 @@ public class Array_2
 	 */
 	public boolean haveThree(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int count = 0;
+		for(int i = 0; i < nums.length; i++)
+		{
+			if(nums[i] == 3)
+			{
+				if((i > 0 && nums[i - 1] == 3) || (i < nums.length - 1 && nums[i + 1] == 3))
+				{
+					return false;
+				}
+				count++;
+			}
+		}
+		return count == 3;
 	}
 
 	/**
