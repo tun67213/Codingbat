1e29067c7b15b5423865d5a3bb0f96c573743826
tun67213 <tun67213@temple.edu>
2024-12-15 09:34:49 -0500

    Implemented method topping2()

--- Java/src/com/arvindhvelrajan/Map_1.java
+++ Java/src/com/arvindhvelrajan/Map_1.java
@@ -119,7 +119,15 @@ public class Map_1
 	 */
 	public Map<String, String> topping2(Map<String, String> map)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(map.containsKey("ice cream"))
+		{
+			map.put("yogurt", map.get("ice cream"));
+		}
+		if(map.containsKey("spinach"))
+		{
+			map.put("spinach", "nuts");
+		}
+		return map;
 	}
 
 	/**
