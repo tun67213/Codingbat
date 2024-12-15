c726172dfdef97a324e631d0af6cb000adeb7878
tun67213 <tun67213@temple.edu>
2024-12-14 19:40:36 -0500

    Added method scores100() to implement

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -27,6 +27,19 @@ public class AP_1
 	}
 
 	/**
+	 * Given an array of scores, return true if there are scores of 100 next to each other in the array.
+	 * The array length will be at least 2.
+	 *
+	 * scores100([1, 100, 100]) → true
+	 * scores100([1, 100, 99, 100]) → false
+	 * scores100([100, 1, 100, 100]) → true
+	 */
+	public boolean scores100(int[] scores)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
