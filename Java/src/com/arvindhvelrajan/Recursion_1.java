52e30ffc2b63e44d9c76c40dd37b0e206f0f4c0e
tun67213 <tun67213@temple.edu>
2024-12-15 07:56:20 -0500

    Implemented method count11()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -464,7 +464,15 @@ public class Recursion_1
 	 */
 	public int count11(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() < 2)
+		{
+			return 0;
+		}
+		if(str.startsWith("11"))
+		{
+			return 1 + count11(str.substring(2));
+		}
+		return count11(str.substring(1));
 	}
 
 	/**
