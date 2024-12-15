9d1728de2b7ce3807f8cb83403ecb425e30dee0e
tun67213 <tun67213@temple.edu>
2024-12-14 22:54:57 -0500

    Implemented method scoresClump()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -69,7 +69,14 @@ public class AP_1
 	 */
 	public boolean scoresClump(int[] scores)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		for(int i = 0; i < scores.length - 2; i++)
+		{
+			if(Math.abs(scores[i] - scores[i + 1]) <= 2 && Math.abs(scores[i] - scores[i + 2]) <= 2 && Math.abs(scores[i + 1] - scores[i + 2]) <= 2)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
