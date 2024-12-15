636d7cc6f879f0f24e6bd2e8404dcee4845b2fa0
tun67213 <tun67213@temple.edu>
2024-12-15 07:58:20 -0500

    Implemented method countHi2()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -506,7 +506,19 @@ public class Recursion_1
 	 */
 	public int countHi2(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() < 2)
+		{
+			return 0;
+		}
+		if(str.startsWith("xhi"))
+		{
+			return countHi2(str.substring(3));
+		}
+		if(str.startsWith("hi"))
+		{
+			return 1 + countHi2(str.substring(2));
+		}
+		return countHi2(str.substring(1));
 	}
 
 	/**
