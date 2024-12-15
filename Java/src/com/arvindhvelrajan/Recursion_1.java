f571710d1f4a087540c5e31fe44b1090d1321ec2
tun67213 <tun67213@temple.edu>
2024-12-15 07:50:37 -0500

    Implemented method array11()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -322,7 +322,15 @@ public class Recursion_1
 	 */
 	public int array11(int[] nums, int index)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(nums.length == 0 || index < 0 || index >= nums.length)
+		{
+			return 0;
+		}
+		if(nums[index] == 11)
+		{
+			return 1 + array11(nums, index + 1);
+		}
+		return array11(nums, index + 1);
 	}
 
 	/**
