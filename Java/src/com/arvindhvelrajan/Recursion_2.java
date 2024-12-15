15f611b02a284272ca67b1a41977630463f482d5
tun67213 <tun67213@temple.edu>
2024-12-15 08:07:38 -0500

    Implemented method groupSum()

--- Java/src/com/arvindhvelrajan/Recursion_2.java
+++ Java/src/com/arvindhvelrajan/Recursion_2.java
@@ -27,7 +27,15 @@ public class Recursion_2
 	 */
 	public boolean groupSum(int start, int[] nums, int target)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(start >= nums.length)
+		{
+			return target == 0;
+		}
+		if(groupSum(start + 1, nums, target - nums[start]))
+		{
+			return true;
+		}
+		return (groupSum(start + 1, nums, target));
 	}
 
 	/**
