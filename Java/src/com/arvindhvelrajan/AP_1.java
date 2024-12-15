d241f0b84d93b50c65e1684b51a4b1e84b3645fe
tun67213 <tun67213@temple.edu>
2024-12-14 21:58:30 -0500

    Added method matchUp()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -169,6 +169,20 @@ public class AP_1
 	}
 
 	/**
+	 * Given 2 arrays that are the same length containing strings, compare the 1st string in one array to the 1st string
+	 * in the other array, the 2nd to the 2nd and so on. Count the number of times that the 2 strings are non-empty and
+	 * start with the same char. The strings may be any length, including 0.
+	 *
+	 * matchUp(["aa", "bb", "cc"], ["aaa", "xx", "bb"]) → 1
+	 * matchUp(["aa", "bb", "cc"], ["aaa", "b", "bb"]) → 2
+	 * matchUp(["aa", "bb", "cc"], ["", "", "ccc"]) → 1
+	 */
+	public int matchUp(String[] a, String[] b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
