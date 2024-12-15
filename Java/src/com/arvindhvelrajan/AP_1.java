257df994062361ebcdb4e1ef042968f2c03f8aa4
tun67213 <tun67213@temple.edu>
2024-12-14 21:38:09 -0500

    Added method copyEvens()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -140,6 +140,19 @@ public class AP_1
 	}
 
 	/**
+	 * Given an array of positive ints, return a new array of length "count" containing the first even numbers from the
+	 * original array. The original array will contain at least "count" even numbers.
+	 *
+	 * copyEvens([3, 2, 4, 5, 8], 2) → [2, 4]
+	 * copyEvens([3, 2, 4, 5, 8], 3) → [2, 4, 8]
+	 * copyEvens([6, 1, 2, 4, 5, 8], 3) → [6, 2, 4]
+	 */
+	public int[] copyEvens(int[] nums, int count)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
