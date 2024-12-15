47f73f89e196bc8c4c59d1b57c8981dca4689cf3
tun67213 <tun67213@temple.edu>
2024-12-14 19:15:44 -0500

    Implemented method fix45()

--- Java/src/com/arvindhvelrajan/Array_3.java
+++ Java/src/com/arvindhvelrajan/Array_3.java
@@ -95,7 +95,29 @@ public class Array_3
 	 */
 	public int[] fix45(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int i = 0;
+		int j = 0;
+
+		while(j < nums.length && nums[j] != 5)
+		{
+			j++;
+		}
+		while(i < nums.length)
+		{
+			if(nums[i] == 4)
+			{
+				int temp = nums[i+1];
+				nums[i+1] = nums[j];
+				nums[j] = temp;
+
+				while((j < nums.length && nums[j] != 5) || j == i + 1)
+				{
+					j++;
+				}
+			}
+			i++;
+		}
+		return nums;
 	}
 
 	/**
