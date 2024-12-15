861474b7f98c807cfcd6a71a24c109d753c8ed66
tun67213 <tun67213@temple.edu>
2024-12-14 22:10:17 -0500

    Added method scoreUp()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -183,6 +183,21 @@ public class AP_1
 	}
 
 	/**
+	 * The "key" array is an array containing the correct answers to an exam, like {"a", "a", "b", "b"}. the "answers"
+	 * array contains a student's answers, with "?" representing a question left blank. The two arrays are not empty and
+	 * are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each
+	 * incorrect answer, and +0 for each blank answer.
+	 *
+	 * scoreUp(["a", "a", "b", "b"], ["a", "c", "b", "c"]) → 6
+	 * scoreUp(["a", "a", "b", "b"], ["a", "a", "b", "c"]) → 11
+	 * scoreUp(["a", "a", "b", "b"], ["a", "a", "b", "b"]) → 16
+	 */
+	public int scoreUp(String[] key, String[] answers)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
