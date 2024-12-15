4eda01d85906fec3b95fc01dceaf6b798b1bfe00
tun67213 <tun67213@temple.edu>
2024-12-14 22:37:37 -0500

    Added method mergeTwo()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -290,6 +290,22 @@ public class AP_1
 	}
 
 	/**
+	 * Start with two arrays of strings, A and B, each with its elements in alphabetical order and without duplicates.
+	 * Return a new array containing the first N elements from the two arrays. The result array should be in alphabetical
+	 * order and without duplicates. A and B will both have a length which is N or more. The best "linear" solution makes
+	 * a single pass over A and B, taking advantage of the fact that they are in alphabetical order, copying elements
+	 * directly to the new array.
+	 *
+	 * mergeTwo(["a", "c", "z"], ["b", "f", "z"], 3) → ["a", "b", "c"]
+	 * mergeTwo(["a", "c", "z"], ["c", "f", "z"], 3) → ["a", "c", "f"]
+	 * mergeTwo(["f", "g", "z"], ["c", "f", "g"], 3) → ["c", "f", "g"]
+	 */
+	public String[] mergeTwo(String[] a, String[] b, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
