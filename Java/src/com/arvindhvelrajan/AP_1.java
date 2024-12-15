1edbae1efda7983ce52c514475d2b8cc3f23d226
tun67213 <tun67213@temple.edu>
2024-12-14 22:21:55 -0500

    Added method scoresSpecial()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -212,6 +212,21 @@ public class AP_1
 	}
 
 	/**
+	 * Given two arrays, A and B, of non-negative int scores. A "special" score is one which is a multiple of 10, such
+	 * as 40 or 90. Return the sum of largest special score in A and the largest special score in B.
+	 * To practice decomposition, write a separate helper method which finds the largest special score in an array.
+	 * Write your helper method after your scoresSpecial() method in the JavaBat text area.
+	 *
+	 * scoresSpecial([12, 10, 4], [2, 20, 30]) → 40
+	 * scoresSpecial([20, 10, 4], [2, 20, 10]) → 40
+	 * scoresSpecial([12, 11, 4], [2, 20, 31]) → 20
+	 */
+	public int scoresSpecial(int[] a, int[] b)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
