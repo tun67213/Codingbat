1de9b1f46db5d90cf14e093fa4b2136eaa3b953b
tun67213 <tun67213@temple.edu>
2024-12-14 19:41:08 -0500

    Added method scoresClump()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -40,6 +40,19 @@ public class AP_1
 	}
 
 	/**
+	 * Given an array of scores sorted in increasing order, return true if the array contains 3 adjacent scores that
+	 * differ from each other by at most 2, such as with {3, 4, 5} or {3, 5, 5}.
+	 *
+	 * scoresClump([3, 4, 5]) → true
+	 * scoresClump([3, 4, 6]) → false
+	 * scoresClump([1, 3, 5, 5]) → true
+	 */
+	public boolean scoresClump(int[] scores)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
