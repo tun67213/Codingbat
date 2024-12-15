5e122c31be57aac4c5fe2e6b78ede4a2818e0d27
tun67213 <tun67213@temple.edu>
2024-12-14 19:34:37 -0500

    Implemented method maxMirror()

--- Java/src/com/arvindhvelrajan/Array_3.java
+++ Java/src/com/arvindhvelrajan/Array_3.java
@@ -251,7 +251,25 @@ public class Array_3
 	 */
 	public int maxMirror(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		int max = 0;
+		for(int i = 0; i < nums.length; i++)
+		{
+			int count = 0;
+			for(int j = nums.length - 1; j >= 0 && i + count < nums.length; j--)
+			{
+				if(nums[i + count] == nums[j])
+				{
+					count++;
+				}
+				else
+				{
+					max = Math.max(max, count);
+					count = 0;
+				}
+			}
+			max = Math.max(max, count);
+		}
+		return max;
 	}
 
 	/**
