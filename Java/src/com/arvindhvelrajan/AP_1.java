5ae255edf8c47895243c33fe2eaeaa58b2afb0f0
tun67213 <tun67213@temple.edu>
2024-12-14 19:40:04 -0500

    Added method scoresIncreasing() for implementing

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -14,6 +14,19 @@ public class AP_1
 	}
 
 	/**
+	 * Given an array of scores, return true if each score is equal or greater than the one before.
+	 * The array will be length 2 or more.
+	 *
+	 * scoresIncreasing([1, 3, 4]) → true
+	 * scoresIncreasing([1, 3, 2]) → false
+	 * scoresIncreasing([1, 1, 4]) → true
+	 */
+	public boolean scoresIncreasing(int[] scores)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
