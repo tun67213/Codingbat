074064666eba7ce37dd83b8b590e6ca6fbdbe49a
tun67213 <tun67213@temple.edu>
2024-12-14 21:28:32 -0500

    Added method wordsWithoutList()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -99,6 +99,19 @@ public class AP_1
 	}
 
 	/**
+	 * Given an array of strings, return a new List (e.g. an ArrayList) where all the strings of the given length are
+	 * omitted. See wordsWithout() below which is more difficult because it uses arrays.
+	 *
+	 * wordsWithoutList(["a", "bb", "b", "ccc"], 1) → ["bb", "ccc"]
+	 * wordsWithoutList(["a", "bb", "b", "ccc"], 3) → ["a", "bb", "b"]
+	 * wordsWithoutList(["a", "bb", "b", "ccc"], 4) → ["a", "bb", "b", "ccc"]
+	 */
+	public List wordsWithoutList(String[] words, int len)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
