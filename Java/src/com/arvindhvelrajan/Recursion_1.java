890b7bcb50b377cbdc26e7d92989b8b5a5d8662d
tun67213 <tun67213@temple.edu>
2024-12-15 07:53:11 -0500

    Implemented method pairStar()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -381,7 +381,15 @@ public class Recursion_1
 	 */
 	public String pairStar(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() <= 1)
+		{
+			return str;
+		}
+		if(str.charAt(0) == str.charAt(1))
+		{
+			return str.charAt(0) + "*" + pairStar(str.substring(1));
+		}
+		return str.charAt(0) + pairStar(str.substring(1));
 	}
 
 	/**
