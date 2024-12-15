9c2e070025ac05e02ac99f7f52d9f1dc33d99865
tun67213 <tun67213@temple.edu>
2024-12-15 14:54:08 -0500

    Implemented method noZ()

--- Java/src/com/arvindhvelrajan/Functional_2.java
+++ Java/src/com/arvindhvelrajan/Functional_2.java
@@ -64,7 +64,8 @@ public class Functional_2
 	 */
 	public List<String> noZ(List<String> strings)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		strings.removeIf(str -> str.contains("z"));
+		return strings;
 	}
 
 	/**
