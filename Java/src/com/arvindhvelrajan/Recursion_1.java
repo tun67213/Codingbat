b886ca062d6791d0be96f68cf77c277b80328713
tun67213 <tun67213@temple.edu>
2024-12-15 07:55:33 -0500

    Implemented method countAbc()

--- Java/src/com/arvindhvelrajan/Recursion_1.java
+++ Java/src/com/arvindhvelrajan/Recursion_1.java
@@ -443,7 +443,15 @@ public class Recursion_1
 	 */
 	public int countAbc(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() < 3)
+		{
+			return 0;
+		}
+		if(str.startsWith("abc") || str.startsWith("aba"))
+		{
+			return 1 + countAbc(str.substring(1));
+		}
+		return countAbc(str.substring(1));
 	}
 
 	/**
