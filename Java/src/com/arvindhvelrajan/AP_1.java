f42d3e2649252fa89ac493cdaaec90ae5d887edd
tun67213 <tun67213@temple.edu>
2024-12-14 21:34:59 -0500

    Added method dividesSelf()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -125,6 +125,21 @@ public class AP_1
 	}
 
 	/**
+	 * We'll say that a positive int divides itself if every digit in the number divides into the number evenly.
+	 * So for example 128 divides itself since 1, 2, and 8 all divide into 128 evenly.
+	 * We'll say that 0 does not divide into anything evenly, so no number with a 0 digit divides itself.
+	 * Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+	 *
+	 * dividesSelf(128) → true
+	 * dividesSelf(12) → true
+	 * dividesSelf(120) → false
+	 */
+	public boolean dividesSelf(int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
