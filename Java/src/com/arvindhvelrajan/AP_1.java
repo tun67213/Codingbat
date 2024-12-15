b8f49531a5245b32fb11159c499da3d7d95938ad
tun67213 <tun67213@temple.edu>
2024-12-15 07:26:32 -0500

    Implemented method userCompare()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -537,7 +537,23 @@ public class AP_1
 	 */
 	public int userCompare(String aName, int aId, String bName, int bId)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(aName.compareTo(bName) < 0)
+		{
+			return -1;
+		}
+		else if(aName.compareTo(bName) > 0)
+		{
+			return 1;
+		}
+		else if(aId < bId)
+		{
+			return -1;
+		}
+		else if(aId > bId)
+		{
+			return 1;
+		}
+		return 0;
 	}
 
 	/**
