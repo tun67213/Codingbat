e32f2f0cf076d45b548b5da7674b2799294226af
tun67213 <tun67213@temple.edu>
2024-12-15 14:23:22 -0500

    Implemented method allSwap()

--- Java/src/com/arvindhvelrajan/Map_2.java
+++ Java/src/com/arvindhvelrajan/Map_2.java
@@ -200,7 +200,26 @@ public class Map_2
 	 */
 	public String[] allSwap(String[] strings)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		Map<String, Integer> map = new HashMap<>();
+		for(int i = 0; i < strings.length; i++)
+		{
+			String key = String.valueOf(strings[i].charAt(0));
+
+			if (map.containsKey(key))
+			{
+				int pos = map.get(key);
+				String tmp = strings[pos];
+				strings[pos] = strings[i];
+				strings[i] = tmp ;
+				// delete
+				map.remove(key);
+			}
+			else
+			{
+				map.put(key, i);
+			}
+		}
+		return strings;
 	}
 
 	/**
