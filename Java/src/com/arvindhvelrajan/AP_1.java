631de120d9a6c3fe87af836d5f8577abe2e60706
tun67213 <tun67213@temple.edu>
2024-12-14 20:06:02 -0500

    Added method wordsFront()

--- Java/src/com/arvindhvelrajan/AP_1.java
+++ Java/src/com/arvindhvelrajan/AP_1.java
@@ -82,6 +82,18 @@ public class AP_1
 	}
 
 	/**
+	 * Given an array of strings, return a new array containing the first N strings. N will be in the range 1..length.
+	 *
+	 * wordsFront(["a", "b", "c", "d"], 1) → ["a"]
+	 * wordsFront(["a", "b", "c", "d"], 2) → ["a", "b"]
+	 * wordsFront(["a", "b", "c", "d"], 3) → ["a", "b", "c"]
+	 */
+	public String[] wordsFront(String[] words, int n)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	/**
 	 * @param args commandline arguments
 	 */
 	public void main(String[] args)
