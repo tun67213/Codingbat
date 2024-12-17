c21441b6c7b3f2f3393dd1cc14e3d8c1f517dad1
tun67213 <tun67213@temple.edu>
2024-12-16 19:44:10 -0500

    Added method sleep_in()

--- Python/Warmup_1.py
+++ Python/Warmup_1.py
@@ -1,4 +1,15 @@
 from UI_Functions import UI_Functions
 
+'''
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. 
+We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True
+'''
+def sleep_in(weekday, vacation):
+    pass
+
 if __name__ == "__main__":
     pass
