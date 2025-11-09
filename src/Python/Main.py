8347fa1b9c739ac81d441cbb6436f60da4980c50
Arvindh Velrajan <tun67213@temple.edu>
2025-11-09 10:46:05 -0500

    Added another welcome statement

--- src/Python/Main.py
+++ src/Python/Main.py
@@ -1,8 +1,12 @@
 import Warmup_1
 
 def welcome1():
-    print("\nWelcome to Codingbat.com Methods for Python!\n")
+    print("\nWelcome to Codingbat.com Method Implementations\n\n")
+
+def welcome2():
+    print("\nPython Implementations\n")
 
 if __name__ == "__main__":
     welcome1()
+    welcome2()
     Warmup_1.main()
