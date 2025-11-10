c04cec5049209dcb067f09c2ebf907f18e334cb0
Arvindh Velrajan <tun67213@temple.edu>
2025-11-09 22:38:37 -0500

    Added List-1 to Main

--- src/Python/Main.py
+++ src/Python/Main.py
@@ -1,4 +1,4 @@
-import Warmup_1, Warmup_2, String_1
+import Warmup_1, Warmup_2, String_1, List_1
 
 def welcome1():
     print("\nWelcome to Codingbat.com Method Implementations\n\n")
@@ -12,3 +12,4 @@ if __name__ == "__main__":
     Warmup_1.main()
     Warmup_2.main()
     String_1.main()
+    List_1.main()
