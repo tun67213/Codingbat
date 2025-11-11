e18eb1e8b8f154ff45ead17cae0a913e24c4765e
Arvindh Velrajan <tun67213@temple.edu>
2025-11-11 10:56:04 -0500

    Implemented method printMethod()

--- src/Java/src/HelperMethods.java
+++ src/Java/src/HelperMethods.java
@@ -4,5 +4,18 @@
  */
 public class HelperMethods
 {
-	
+	protected static int callTimes = 0;
+
+	public static void printMethod(String methodName)
+	{
+		if(callTimes > 0)
+		{
+			System.out.println("\nMethod Name: " + methodName + "()\n");
+		}
+		else
+		{
+			System.out.println("Method Name: " + methodName + "()\n");
+		}
+		callTimes++;
+	}
 }
