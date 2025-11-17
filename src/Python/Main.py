3b1856014afef1d4c340d60f188646366860669b
Arvindh Velrajan <tun67213@temple.edu>
2025-11-16 23:28:12 -0500

    Updated file

--- src/Python/Main.py
+++ src/Python/Main.py
@@ -5,4 +5,14 @@ def welcome():
 
 if __name__ == "__main__":
     welcome()
+
+def welcome1():
+    print("\nWelcome to Codingbat.com Method Implementations\n\n")
+
+def welcome2():
+    print("\nPython Implementations\n")
+
+if __name__ == "__main__":
+    welcome1()
+    welcome2()
     Warmup_1.main()
