ad8e1b28bb8f7bb6ad83708f7e0c248dc892b643
Arvindh Velrajan <tun67213@temple.edu>
2025-11-19 22:47:47 -0500

    Added missing method everyNth()

--- src/Java/src/Warmup_1.java
+++ src/Java/src/Warmup_1.java
@@ -396,6 +396,18 @@ public class Warmup_1 extends HelperMethods
 	}
 
 	/**
+	 * Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+	 *
+	 * everyNth("Miracle", 2) → "Mrce"
+	 * everyNth("abcdefg", 2) → "aceg"
+	 * everyNth("abcdefg", 3) → "adg"
+	 */
+	public String everyNth(String str, int n)
+	{
+		throw new UnsupportedOperationException("Not implemented yet");
+	}
+
+	/**
 	 * @param args command line arguments
 	 */
 	public void main(String[] args)
