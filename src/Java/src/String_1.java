f3900776b1a845abd4088bf0446d1a4d5c1f931e
Arvindh Velrajan <tun67213@temple.edu>
2025-11-20 11:45:34 -0500

    Added welcome() method

--- src/Java/src/String_1.java
+++ src/Java/src/String_1.java
@@ -6,10 +6,19 @@ package src;
 public class String_1 extends HelperMethods
 {
 	/**
+	 * This method welcomes the user to this program.
+	 */
+	public void welcome()
+	{
+		System.out.println("\n\nString-1 Methods\n");
+	}
+
+	/**
 	 * @param args command line arguments
 	 */
 	public void main(String[] args)
 	{
-
+		callTimes = 0;
+		welcome();
 	}
 }
