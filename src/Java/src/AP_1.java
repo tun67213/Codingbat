fad05dd33ff39d7a61f52c336e3e0c85e68d58b3
Arvindh Velrajan <tun67213@temple.edu>
2026-01-10 15:22:03 +0900

    Included and called welcome()

--- src/Java/src/AP_1.java
+++ src/Java/src/AP_1.java
@@ -6,10 +6,18 @@ package src;
 public class AP_1 extends HelperMethods
 {
 	/**
+	 * This method welcomes the user to this program.
+	 */
+	public void welcome()
+	{
+		System.out.println("AP-1 Methods\n");
+	}
+
+	/**
 	 * @param args command line arguments
 	 */
 	public void main(String[] args)
 	{
-
+		welcome();
 	}
 }
