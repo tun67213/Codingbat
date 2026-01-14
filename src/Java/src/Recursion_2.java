5f7904609ebb8e4e0ec082bc461a42e5dbd779c8
Arvindh Velrajan <tun67213@temple.edu>
2026-01-14 16:05:09 +0900

    Added welcome() method and called it in main()

--- src/Java/src/Recursion_2.java
+++ src/Java/src/Recursion_2.java
@@ -6,10 +6,18 @@ package src;
 public class Recursion_2 extends HelperMethods
 {
 	/**
+	 * This method welcomes the user to this program.
+	 */
+	public void welcome()
+	{
+		System.out.println("\n\nRecursion-2 Methods\n");
+	}
+
+	/**
 	 * @param args command line arguments
 	 */
 	public void main(String[] args)
 	{
-
+		welcome();
 	}
 }
