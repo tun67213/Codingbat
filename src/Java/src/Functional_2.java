75ec566eb898ed430fff0ad35d8a9070c2f9aa91
Arvindh Velrajan <tun67213@temple.edu>
2026-01-31 08:39:52 +0900

    Added and implemented welcome() method

--- src/Java/src/Functional_2.java
+++ src/Java/src/Functional_2.java
@@ -6,6 +6,14 @@ package src;
 public class Functional_2 extends HelperMethods
 {
 	/**
+	 * This method welcomes the user to this program.
+	 */
+	public void welcome()
+	{
+		System.out.println("\n\nFunctional-2 Methods\n");
+	}
+
+	/**
 	 * @param args command line arguments
 	 */
 	public void main(String[] args)
