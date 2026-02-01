4b404da2202edc4cb6c2d8d953852486e0a67c5b
Arvindh Velrajan <tun67213@temple.edu>
2026-02-01 22:13:55 +0900

    Implemented method firstTwo()
    Implemented firstTwo() and successfully ran all test cases associated with the method.

--- src/Java/src/String_1.java
+++ src/Java/src/String_1.java
@@ -87,7 +87,11 @@ public class String_1 extends HelperMethods
 	 */
 	public String firstTwo(String str)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(str.length() <= 2)
+		{
+			return str;
+		}
+		return str.substring(0, 2);
 	}
 
 	/**
