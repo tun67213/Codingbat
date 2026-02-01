5d216e5f258cbaa4c202e067146466f2b4753b8e
Arvindh Velrajan <tun67213@temple.edu>
2026-02-01 10:36:52 +0900

    Implemented and tested last2()
    Implemented last2() and confirmed test case correctness.

--- src/Java/src/Warmup_2.java
+++ src/Java/src/Warmup_2.java
@@ -179,7 +179,20 @@ public class Warmup_2 extends HelperMethods
 	 */
 	public int last2(String str)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(str.length() <= 2)
+		{
+			return 0;
+		}
+		int count = 0;
+		String last = str.substring(str.length() - 2);
+		for(int i = 0; i < str.length() - 2; i++)
+		{
+			if(str.substring(i, i + 2).equals(last))
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
