26672111eff7427cc17b1bd3a613543ea6ff78bc
Arvindh Velrajan <tun67213@temple.edu>
2026-02-01 09:37:51 +0900

    Implemented countXX()
    Implemented iterative version of countXX().

--- src/Java/src/Warmup_2.java
+++ src/Java/src/Warmup_2.java
@@ -61,7 +61,15 @@ public class Warmup_2 extends HelperMethods
 	 */
 	public int countXX(String str)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		int count = 0;
+		for(int i = 0; i < str.length() - 1; i++)
+		{
+			if(str.substring(i, i + 2).equals("xx"))
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
