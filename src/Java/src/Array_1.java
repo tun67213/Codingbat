512f157a08bd2dfb96be73d9282afba1b7e93d97
Arvindh Velrajan <tun67213@temple.edu>
2026-02-04 17:18:48 +0900

    Implemented method swapEnds()
    Implemented swapEnds() and successfully ran all test cases.

--- src/Java/src/Array_1.java
+++ src/Java/src/Array_1.java
@@ -402,7 +402,14 @@ public class Array_1 extends HelperMethods
 	 */
 	public int[] swapEnds(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(nums.length < 2)
+		{
+			return nums;
+		}
+		int temp = nums[0];
+		nums[0] = nums[nums.length - 1];
+		nums[nums.length - 1] = temp;
+		return nums;
 	}
 
 	/**
