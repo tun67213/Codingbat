bfa29c371089ef713d2eb8430806b0fdf72d930b
Arvindh Velrajan <tun67213@temple.edu>
2026-02-04 17:33:34 +0900

    Implemented method make2()
    Implemented make2() and successfully ran all test cases.

--- src/Java/src/Array_1.java
+++ src/Java/src/Array_1.java
@@ -490,7 +490,23 @@ public class Array_1 extends HelperMethods
 	 */
 	public int[] make2(int[] a, int[] b)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(a.length >= 2)
+		{
+			return new int[] {a[0], a[1]};
+		}
+		if(a.length >= 1)
+		{
+			if(b.length >= 1)
+			{
+				return new int[] {a[0], b[0]};
+			}
+			return a;
+		}
+		if(b.length >= 2)
+		{
+			return new int[] {b[0], b[1]};
+		}
+		return b;
 	}
 
 	/**
