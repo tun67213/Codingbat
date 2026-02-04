bb0d3bf287a7207f464c8a32801969803731a147
Arvindh Velrajan <tun67213@temple.edu>
2026-02-04 17:29:41 +0900

    Implemented method unlucky1()
    Implemented unlucky1() and successfully ran all test cases.

--- src/Java/src/Array_1.java
+++ src/Java/src/Array_1.java
@@ -474,7 +474,11 @@ public class Array_1 extends HelperMethods
 	 */
 	public boolean unlucky1(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if((nums.length >= 2 && ((nums[0] == 1 && nums[1] == 3) || (nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3))) || (nums.length >= 3 && ((nums[1] == 1 && nums[2] == 3) || (nums[nums.length - 3] == 1 && nums[nums.length - 2] == 3))))
+		{
+			return true;
+		}
+		return false;
 	}
 
 	/**
