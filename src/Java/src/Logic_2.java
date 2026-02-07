decbf7e5303f00251f0c3053a11b429ab7ef0e04
Arvindh Velrajan <tun67213@temple.edu>
2026-02-07 10:54:56 +0900

    Implement evenlySpaced
    Implemented evenlySpaced and successfully ran all test cases.

--- src/Java/src/Logic_2.java
+++ src/Java/src/Logic_2.java
@@ -192,7 +192,25 @@ public class Logic_2 extends HelperMethods
 	 */
 	public boolean evenlySpaced(int a, int b, int c)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		int min = a, max = a;
+		if(b < min)
+		{
+			min = b;
+		}
+		if(c < min)
+		{
+			min = c;
+		}
+		if(b > max)
+		{
+			max = b;
+		}
+		if(c > max)
+		{
+			max = c;
+		}
+		int mid = a + b + c - min - max;
+		return mid - min == max - mid;
 	}
 
 	/**
