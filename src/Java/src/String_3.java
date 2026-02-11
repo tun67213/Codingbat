1cb3f22c0c7bc69e260ba23ebc2caba5b67dc1cb
Arvindh Velrajan <tun67213@temple.edu>
2026-02-12 07:48:47 +0900

    Implement countTriple
    Implement countTriple and run all test cases.

--- src/Java/src/String_3.java
+++ src/Java/src/String_3.java
@@ -184,7 +184,19 @@ public class String_3 extends HelperMethods
 	 */
 	public int countTriple(String str)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(str.length() < 3)
+		{
+			return 0;
+		}
+		int count = 0;
+		for(int i = 0; i < str.length() - 2; i++)
+		{
+			if(str.substring(i, i + 1).equals(str.substring(i + 1, i + 2)) && str.substring(i, i + 1).equals(str.substring(i + 2, i + 3)))
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
