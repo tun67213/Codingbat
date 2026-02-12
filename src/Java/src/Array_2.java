616b3e99eb9040b6dbd1ee97f9b07148fea2313b
Arvindh Velrajan <tun67213@temple.edu>
2026-02-12 14:06:39 +0900

    Implement haveThree

--- src/Java/src/Array_2.java
+++ src/Java/src/Array_2.java
@@ -464,7 +464,19 @@ public class Array_2 extends HelperMethods
 	 */
 	public boolean haveThree(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		int count = 0;
+		for(int i = 0; i < nums.length; i++)
+		{
+			if(nums[i] == 3)
+			{
+				count++;
+				if(i > 0 && nums[i - 1] == 3)
+				{
+					return false;
+				}
+			}
+		}
+		return count == 3;
 	}
 
 	/**
