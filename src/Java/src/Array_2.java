57870eaddbaf81126ad8bb35a3347b3df0c6a5ff
Arvindh Velrajan <tun67213@temple.edu>
2026-02-12 13:10:12 +0900

    Implement isEverywhere
    Implement isEverywhere and make sure it abides to the contract. Also, make sure that the test cases generated show the correct results. Otherwise, go back and redo it.

--- src/Java/src/Array_2.java
+++ src/Java/src/Array_2.java
@@ -317,7 +317,18 @@ public class Array_2 extends HelperMethods
 	 */
 	public boolean isEverywhere(int[] nums, int val)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(nums.length <= 1)
+		{
+			return true;
+		}
+		for(int i = 0; i < nums.length - 1; i++)
+		{
+			if(nums[i] != val && nums[i + 1] != val)
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
