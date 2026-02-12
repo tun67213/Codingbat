c4a8f892dc4db80e786a2e082bf3d6663124a525
Arvindh Velrajan <tun67213@temple.edu>
2026-02-12 13:57:46 +0900

    Implement modThree
    Implement modThree and make sure you can successfully run all the test cases properly.

--- src/Java/src/Array_2.java
+++ src/Java/src/Array_2.java
@@ -437,7 +437,22 @@ public class Array_2 extends HelperMethods
 	 */
 	public boolean modThree(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(nums.length < 3)
+		{
+			return false;
+		}
+		for(int i = 0; i < nums.length - 2; i++)
+		{
+			if((nums[i] % 2 == 0) && (nums[i + 1] % 2 == 0) && (nums[i + 2] % 2 == 0))
+			{
+				return true;
+			}
+			if((nums[i] % 2 == 1) && (nums[i + 1] % 2 == 1) && (nums[i + 2] % 2 == 1))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
