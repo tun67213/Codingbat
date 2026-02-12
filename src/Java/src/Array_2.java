2f815f7e8639d0018b52641fe15a8cd479c45f8b
Arvindh Velrajan <tun67213@temple.edu>
2026-02-12 13:47:18 +0900

    Implement has77
    Implement has77 and make sure test cases run successfully.

--- src/Java/src/Array_2.java
+++ src/Java/src/Array_2.java
@@ -390,7 +390,18 @@ public class Array_2 extends HelperMethods
 	 */
 	public boolean has77(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		for(int i = 0; i < nums.length - 1; i++)
+		{
+			if(nums[i] == 7 && nums[i + 1] == 7)
+			{
+				return true;
+			}
+			if(nums[i] == 7 && i + 2 < nums.length && nums[i + 2] == 7)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
