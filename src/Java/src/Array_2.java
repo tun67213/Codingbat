fec372e44ed05f2230f7fbdfacd923fb4acd6d3f
Arvindh Velrajan <tun67213@temple.edu>
2026-02-12 09:35:59 +0900

    Implement has22
    Implement has22 and run test cases to ensure no errors are generated.

--- src/Java/src/Array_2.java
+++ src/Java/src/Array_2.java
@@ -153,7 +153,18 @@ public class Array_2 extends HelperMethods
 	 */
 	public boolean has22(int[] nums)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(nums.length < 2)
+		{
+			return false;
+		}
+		for(int i = 0; i < nums.length - 1; i++)
+		{
+			if(nums[i] == 2 && nums[i + 1] == 2)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
