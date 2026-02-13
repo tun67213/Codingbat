eb0f5954cc50f41fa364e4cc4edc64f503eb04d1
Arvindh Velrajan <tun67213@temple.edu>
2026-02-13 19:06:06 +0900

    Implement scoresClump
    Implement scoresClump and confirm that all test cases run successfully, and that there are no hidden errors in there.

--- src/Java/src/AP_1.java
+++ src/Java/src/AP_1.java
@@ -67,7 +67,18 @@ public class AP_1 extends HelperMethods
 	 */
 	public boolean scoresClump(int[] scores)
 	{
-		throw new UnsupportedOperationException("Not implemented yet");
+		if(scores.length <= 2)
+		{
+			return false;
+		}
+		for(int i = 0; i < scores.length - 2; i++)
+		{
+			if((Math.abs(scores[i] - scores[i + 1]) <= 2) && (Math.abs(scores[i] - scores[i + 2]) <= 2) && (Math.abs(scores[i + 1] - scores[i + 2]) <= 2))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
