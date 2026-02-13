5e707b79db2e34e001c1106ca676b2ebec5a973e
Arvindh Velrajan <tun67213@temple.edu>
2026-02-13 20:35:41 +0900

    Add helper method
    Add a helper method to convert a list to String for method wordsWithout.

--- src/Java/src/AP_1.java
+++ src/Java/src/AP_1.java
@@ -365,6 +365,15 @@ public class AP_1 extends HelperMethods
 	{
 		throw new UnsupportedOperationException("Not implemented yet");
 	}
+	private String[] listToString(List<String> list)
+	{
+		String[] newArray = new String[list.size()];
+		for(int i = 0; i < list.size(); i++)
+		{
+			newArray[i] = list.get(i);
+		}
+		return newArray;
+	}
 
 	/**
 	 * Given two arrays, A and B, of non-negative int scores. A "special" score is one which is a multiple of 10, such as 40 or 90. Return the sum of largest special score in A and the largest special score in B. To practice decomposition, write a separate helper method which finds the largest special score in an array. Write your helper method after your scoresSpecial() method in the JavaBat text area.
