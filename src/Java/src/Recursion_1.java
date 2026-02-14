3c24848e33679170c160857fafdb38bc9784962d
Arvindh Velrajan <tun67213@temple.edu>
2026-02-14 10:00:13 +0900

    Implement stringClean
    Implement method stringClean and ensure that all test cases pass.

--- src/Java/src/Recursion_1.java
+++ src/Java/src/Recursion_1.java
@@ -458,7 +458,15 @@ public class Recursion_1 extends HelperMethods
 	 */
 	public String stringClean(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() <= 1)
+		{
+			return str;
+		}
+		if(str.substring(0, 1).equals(str.substring(1, 2)))
+		{
+			return stringClean(str.substring(1));
+		}
+		return str.substring(0, 1) + stringClean(str.substring(1));
 	}
 
 	/**
