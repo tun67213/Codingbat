e1018fab3fa6631bf9c279828fa730e2768422cd
Arvindh Velrajan <tun67213@temple.edu>
2026-02-15 08:34:34 +0900

    Implement mapAB4
    Implement method mapAB4 and ensure all test cases run with no errors generated nor any bugs.

--- src/Java/src/Map_1.java
+++ src/Java/src/Map_1.java
@@ -175,7 +175,25 @@ public class Map_1 extends HelperMethods
 	 */
 	public Map<String, String> mapAB4(Map<String, String> map)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(map.containsKey("a") && map.containsKey("b"))
+		{
+			String a = map.get("a");
+			String b = map.get("b");
+			if(a.length() > b.length())
+			{
+				map.put("c", a);
+			}
+			else if(b.length() > a.length())
+			{
+				map.put("c", b);
+			}
+			else
+			{
+				map.put("a", "");
+				map.put("b", "");
+			}
+		}
+		return map;
 	}
 
 	/**
