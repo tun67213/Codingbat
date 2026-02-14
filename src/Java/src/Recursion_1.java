5bad9130d9f5eccce30087a76d174617b964c4fa
Arvindh Velrajan <tun67213@temple.edu>
2026-02-14 10:02:16 +0900

    Implement method countHi2
    Implement countHi2 and successfully run all test cases.

--- src/Java/src/Recursion_1.java
+++ src/Java/src/Recursion_1.java
@@ -478,7 +478,19 @@ public class Recursion_1 extends HelperMethods
 	 */
 	public int countHi2(String str)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		if(str.length() < 2)
+		{
+			return 0;
+		}
+		if(str.length() >= 3 && str.substring(0, 3).equals("xhi"))
+		{
+			return countHi2(str.substring(3));
+		}
+		if(str.substring(0, 2).equals("hi"))
+		{
+			return 1 + countHi2(str.substring(2));
+		}
+		return countHi2(str.substring(1));
 	}
 
 	/**
