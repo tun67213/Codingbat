0fb26b894be667529e0006b073f34a493941102a
Arvindh Velrajan <tun67213@temple.edu>
2026-02-15 09:58:02 +0900

    Implement square56
    Implement method square56 and ensure all test cases run successfully.

--- src/Java/src/Functional_2.java
+++ src/Java/src/Functional_2.java
@@ -130,7 +130,9 @@ public class Functional_2 extends HelperMethods
 	 */
 	public List<Integer> square56(List<Integer> nums)
 	{
-		throw new UnsupportedOperationException("Not supported yet.");
+		nums.replaceAll(num -> num * num + 10);
+		nums.removeIf(num -> (num % 10 == 5 || num % 10 == 6));
+		return nums;
 	}
 
 	/**
