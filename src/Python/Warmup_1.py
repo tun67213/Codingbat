add1f79396661d07961424ed71ba83f8b5ae01ea
Arvindh Velrajan <tun67213@temple.edu>
2026-04-14 11:32:11 +0530

    Implement sleep_in
    Implement the method sleep_in and ensure that all test cases run successfully.

--- src/Python/Warmup_1.py
+++ src/Python/Warmup_1.py
@@ -14,7 +14,9 @@ sleep_in(True, False) → False
 sleep_in(False, True) → True
 """
 def sleep_in(weekday: bool, vacation: bool) -> bool:
-    raise NotImplementedError("sleep_in not implemented yet")
+    if vacation or not weekday:
+        return True
+    return False
 
 """
 We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
