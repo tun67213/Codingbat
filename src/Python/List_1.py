c003050b603e4eb729b3e8c2d4104b7190bcfdad
Arvindh Velrajan <tun67213@temple.edu>
2026-04-21 08:12:42 +0900

    Implement rotate_left3
    Implement the method and ensure that the test cases return the appropriate and correct values.

--- src/Python/List_1.py
+++ src/Python/List_1.py
@@ -70,7 +70,11 @@ rotate_left3([5, 11, 9]) → [11, 9, 5]
 rotate_left3([7, 0, 0]) → [0, 0, 7]
 """
 def rotate_left3(nums: list[int]) -> list[int]:
-    raise NotImplementedError("rotate_left3 not implemented yet")
+    temporary = nums[0]
+    for i in range(0, len(nums) - 1):
+        nums[i] = nums[i + 1]
+    nums[len(nums) - 1] = temporary
+    return nums
 
 """
 Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
