feada6a2e6f1be83c85865f5dc1797f6584b1bde
Arvindh Velrajan <tun67213@temple.edu>
2026-04-22 08:44:03 +0900

    Update Logic_1.py
    Implement method squirrel_play and ensure call test cases run.

--- src/Python/Logic_1.py
+++ src/Python/Logic_1.py
@@ -42,7 +42,12 @@ squirrel_play(95, False) → False
 squirrel_play(95, True) → True
 """
 def squirrel_play(temp: int, is_summer: bool) -> bool:
-    raise NotImplementedError("squirrel_play not implemented yet")
+    if temp < 60:
+        return False
+    elif is_summer:
+        return temp <= 100
+    else:
+        return temp <= 90
 
 """
 You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
