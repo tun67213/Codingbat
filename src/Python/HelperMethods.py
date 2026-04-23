41db4b654838751a5ae8e80e29d4ac9beda23732
Arvindh Velrajan <tun67213@temple.edu>
2026-04-23 11:17:43 +0900

    Update HelperMethods.py
    Update variable __callTimes to _callTimes to avoid unnecessary hidden behaviors.

--- src/Python/HelperMethods.py
+++ src/Python/HelperMethods.py
@@ -1,15 +1,15 @@
 
 class HelperMethods:
-    __callTimes = 0
+    _callTimes = 0
     
     @classmethod
     def resetCallTimes(cls):
-        cls.__callTimes = 0
+        cls._callTimes = 0
     
     @classmethod
     def printMethod(cls, methodName: str):
-        if cls.__callTimes > 0:
+        if cls._callTimes > 0:
             print("\n\nMethod Name: " + methodName + "()\n")
         else:
             print("Method Name: " + methodName + "()\n")
-        cls.__callTimes += 1
+        cls._callTimes += 1
